d11dc86fef23db09728d7e729284f5657d71774a
thegurlalsingh <g0390076@gmail.com>
2025-01-18 11:58:50 +0530

    .

---
+++ 1319. Number of Operations to Make Network Connected.cpp
@@ -0,0 +1,56 @@
+class DisjointSet {
+    vector<int> rank, parent;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return; 
+
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(n-1 > connections.size()){
+            return -1;
+        }
+        DisjointSet ds(n);
+        int count = 0;
+        for (auto& edge : connections) {
+            int u = edge[0], v = edge[1];
+            ds.unionByRank(u, v);
+        }
+        for(int i = 0; i < n; i++){
+            if(ds.findUPar(i) == i){
+                count++;
+            }
+        }
+
+        return count-1; 
+    }
+};
---
+++ 684. Redundant Connection.cpp
@@ -0,0 +1,54 @@
+class DisjointSet {
+    vector<int> rank, parent;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return; 
+
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        DisjointSet ds(n);
+
+        for (auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+
+            if (ds.findUPar(u) == ds.findUPar(v)) {
+                return edge; 
+            }
+
+            ds.unionByRank(u, v);
+        }
+
+        return {}; 
+    }
+};
