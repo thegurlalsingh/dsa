ad75f14312ae77b31ef5cb6baead950f74358b10
thegurlalsingh <g0390076@gmail.com>
2025-03-15 19:27:22 +0530

    Time: 416 ms (91.87%) | Memory: 240.5 MB (66.17%) - LeetSync

---
+++ 3786-longest-palindromic-subsequence-after-at-most-k-operations/longest-palindromic-subsequence-after-at-most-k-operations.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    int solve(int i, int j, int k, string& s, vector<vector<vector<int>>>& dp) {
+        if (i > j) {
+            return 0;
+        }
+        if (i == j) {
+            return 1;  
+        }
+
+        if (dp[i][j][k] != -1) {
+            return dp[i][j][k];
+        }
+
+        if (s[i] == s[j]) {
+            return dp[i][j][k] = 2 + solve(i + 1, j - 1, k, s, dp);
+        }
+
+        int res = max(solve(i + 1, j, k, s, dp), solve(i, j - 1, k, s, dp));
+
+        int cost = min(abs(s[i] - s[j]), 26 - abs(s[i] - s[j]));
+        if (k >= cost) {
+            res = max(res, 2 + solve(i + 1, j - 1, k - cost, s, dp));
+        }
+
+        return dp[i][j][k] = res;
+    }
+
+public:
+    int longestPalindromicSubsequence(string s, int k) {
+        int n = s.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(k + 1, -1)));
+        return solve(0, n - 1, k, s, dp);
+    }
+};
