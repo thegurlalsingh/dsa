fe1a070e1c148a85aa680c04c65897403bb8482c
thegurlalsingh <g0390076@gmail.com>
2025-04-27 17:55:51 +0530

    Time: 74 ms (21.59%) | Memory: 252.1 MB (10.06%) - LeetSync

---
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -0,0 +1,66 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class DisjointSet {
+    vector<int> rank, parent;
+public:
+    DisjointSet(int n) {
+        rank.resize(n, 0);
+        parent.resize(n);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        DisjointSet ds(n);
+        
+        vector<pair<int,int>> arr;
+        for(int i = 0; i < n; i++) {
+            arr.push_back({nums[i], i});
+        }
+        sort(arr.begin(), arr.end());  
+
+        for(int i = 0; i < n - 1; i++) {
+            if (arr[i+1].first - arr[i].first <= maxDiff) {
+                ds.unionByRank(arr[i].second, arr[i+1].second);
+            }
+        }
+        
+        vector<bool> res;
+        for(auto& q : queries) {
+            if(ds.findUPar(q[0]) == ds.findUPar(q[1]))
+                res.push_back(true);
+            else
+                res.push_back(false);
+        }
+        
+        return res;
+    }
+};
