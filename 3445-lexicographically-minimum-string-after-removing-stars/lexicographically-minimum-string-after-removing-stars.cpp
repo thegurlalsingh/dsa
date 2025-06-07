70820202ac85bc0c481db4ace98e698dd87828ea
thegurlalsingh <g0390076@gmail.com>
2025-06-07 12:02:37 +0530

    Time: 149 ms (43.50%) | Memory: 25.9 MB (78.90%) - LeetSync

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    typedef pair<char,int> P;
+    struct comp{
+        bool operator()(P &p1,P& p2){
+            if(p1.first==p2.first){
+                return p1.second<p2.second;
+            }
+            return p1.first>p2.first;
+        }
+    };
+public:
+    string clearStars(string s) {
+        int n=s.length();
+        priority_queue<P,vector<P>,comp> pq;
+
+        for(int i=0;i<n;i++){
+            if(s[i]!='*'){
+                pq.push({s[i],i});
+            }
+            else{
+                int idx=pq.top().second;
+                pq.pop();
+                s[idx]='*';
+            }
+        }
+        string result="";
+        for(int i=0;i<n;i++){
+            if(s[i]!='*'){
+                result.push_back(s[i]);
+            }
+        }
+        return result;
+    }
+};
