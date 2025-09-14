be5dcb81c0a6d642b454206aa69a261f7d2a595a
thegurlalsingh <g0390076@gmail.com>
2025-09-14 19:00:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (75.26%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,72 +1,47 @@
 class Solution {
 public:
-    bool isvalid(int x, int y, int n, int m, vector<vector<int>>& grid){
-        if(x>=0 && x<n && y>=0 && y<m && grid[x][y]==1){
-            return true;
-        }
-
-        return false;
-    }
+    struct Node {
+        int r, c, t;
+    };
 
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        int time = 0;
+        int n = grid.size(), m = grid[0].size();
+        queue<Node> q;
         int fresh = 0;
-        queue<pair<int, int>> q;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==2){
-                    q.push({i,j});
-                }else if(grid[i][j]==1){
+
+        // 1. Add rotten oranges & count fresh
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 2){
+                    q.push({i,j,0});
+                }
+                if(grid[i][j] == 1){
                     fresh++;
                 }
             }
         }
-        if(fresh==0) return 0;
 
+        int dRow[4] = {-1,1,0,0};
+        int dCol[4] = {0,0,-1,1};
+        int maxTime = 0, rotten = 0;
+
+        // 2. BFS
         while(!q.empty()){
-            int size = q.size();
-            int temp = 0;
-            while(size!=0){
-                pair<int, int>p = q.front();
-                q.pop();
+            auto curr = q.front(); q.pop();
+            maxTime = max(maxTime, curr.t);
 
-                int x1=p.first;
-                int y1=p.second;
-
-                int ax[4] = {1, -1, 0, 0};
-                int ay[4] = {0, 0, 1, -1};
-
-                for(int i=0; i<4; i++){
-                    int x = ax[i]+x1;
-                    int y = ay[i]+y1;
-
-                    if(isvalid(x, y, n, m, grid)){
-                        temp++;
-                        grid[x][y]=2;
-                        q.push({x,y});
-                    }
-                }
-                size--;
-            }
-            if(temp!=0) time++;
-        }
-
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==1){
-                    time = 0;
+            for(int k=0;k<4;k++){
+                int nr = curr.r + dRow[k];
+                int nc = curr.c + dCol[k];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc] == 1){
+                    grid[nr][nc] = 2;
+                    q.push({nr,nc,curr.t+1});
+                    rotten++;
                 }
             }
         }
 
-        if(time==0){
-            return -1;
-        }
-
-        return time;
+        // 3. Check if all fresh rotted
+        return rotten == fresh ? maxTime : -1;
     }
 };
