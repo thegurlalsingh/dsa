3cd7c36faa3c632483cbd583eb9bac62bc208ce4
thegurlalsingh <g0390076@gmail.com>
2025-10-28 15:37:52 +0530

    Time: 4 ms (52.35%) | Memory: 17.9 MB (28.95%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,30 +1,26 @@
- class Solution {
+class Solution {
+    int dfs(int i, int n, vector<int> &nums, unordered_map<int, int> &m, vector<int> &dp) {
+        if(i == n) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int notpick = 0 + dfs(i + 1, n, nums, m, dp);
+        int pick = m[nums[i]] * nums[i];
+        if(i + 1 < n && nums[i] + 1 == nums[i + 1]) pick += dfs(i + 2, n, nums, m, dp);
+        else if(i + 1 < n && nums[i] + 1 != nums[i + 1]) pick += dfs(i + 1, n, nums, m, dp);
+        
+        return dp[i] = max(pick, notpick);
+    }
 public:
     int deleteAndEarn(vector<int>& nums) {
-        if(nums.size() == 0) return 0;
+        unordered_map<int, int> m;
+        for(int i : nums) m[i]++;
 
-        unordered_map<int, int> totalSum;
-        for(auto num: nums) totalSum[num] += num;
+        vector<int> v;
+        for(auto i : m) v.push_back(i.first);
+        sort(v.begin(), v.end());
+        int n = v.size();
+        vector<int> dp(n, -1);
 
-        vector<int> unique;
-        for(auto sum: totalSum) unique.push_back(sum.first);
-
-        sort(unique.begin(), unique.end());
-        int n = unique.size();
-        
-        vector<int> dp(n, 0);
-        dp[0] = totalSum[unique[0]];
-        
-        for(int i=1; i<n; i++){
-            if(unique[i] == unique[i-1] + 1){
-                int take = totalSum[unique[i]] + (i>=2 ? dp[i-2] : 0);
-                dp[i] = max(dp[i-1], take);
-            }
-            else{
-                dp[i] = dp[i-1] + totalSum[unique[i]];
-            }
-        }
-
-        return dp[n-1];
+        return dfs(0, n, v, m, dp);
     }
 };
