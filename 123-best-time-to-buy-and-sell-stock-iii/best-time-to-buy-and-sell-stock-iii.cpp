ed04197cc6986658d19bdb63de13f7f0e564038e
thegurlalsingh <g0390076@gmail.com>
2025-10-30 17:50:25 +0530

    Time: 436 ms (29.76%) | Memory: 211.5 MB (37.62%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,29 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
-private:
-    int maxProfitHelper(vector<int>& prices, int index, int capping, int holding, vector<vector<vector<int>>>& dp) {
-        if (index == prices.size() || capping == 0) {
-            return 0; 
-        }
-        if (dp[index][capping][holding] != -1) {
-            return dp[index][capping][holding];
-        }
-        int doNothing = maxProfitHelper(prices, index + 1, capping, holding, dp);
-        if (holding) {
-            int sell = prices[index] + maxProfitHelper(prices, index + 1, capping - 1, 0, dp);
-            return dp[index][capping][holding] = max(doNothing, sell);
-        } else {
-            int buy = -prices[index] + maxProfitHelper(prices, index + 1, capping, 1, dp);
-            return dp[index][capping][holding] = max(doNothing, buy);
-        }
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(3, vector<int>(2, -1))); 
-        return maxProfitHelper(prices, 0, 2, 0, dp);
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int bought = 0; bought <= 1; bought++) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    int skip = dp[i + 1][bought][cap];
+                    int buy = 0, sell = 0;
+
+                    if (!bought)
+                        buy = -prices[i] + dp[i + 1][1][cap];
+                    else
+                        sell = prices[i] + dp[i + 1][0][cap - 1];
+
+                    dp[i][bought][cap] = max({skip, buy, sell});
+                }
+            }
+        }
+
+        return dp[0][0][2];
     }
 };
