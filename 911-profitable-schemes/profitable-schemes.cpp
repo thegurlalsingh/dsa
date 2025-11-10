79ddbd6af44e9c34ecd5a77e44f64691d2954bcc
thegurlalsingh <g0390076@gmail.com>
2025-11-10 21:37:00 +0530

    Time: 81 ms (72.10%) | Memory: 16.2 MB (51.64%) - LeetSync

---
+++ 911-profitable-schemes/profitable-schemes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int dp[105][105][105]; // dp[i][members][p]
+    const int MOD = 1e9 + 7;
+
+    int solve(int n, vector<int>& group, int i, int minProfit, vector<int>& profit, int p, int members) {
+        if (i >= group.size()) {
+            return p >= minProfit ? 1 : 0;
+        }
+
+        if (dp[i][members][p] != -1)
+            return dp[i][members][p];
+
+        int take = 0;
+        if (members + group[i] <= n) {
+            take = solve(n, group, i + 1, minProfit, profit, min(p + profit[i], minProfit), members + group[i]);
+        }
+
+        int notTake = solve(n, group, i + 1, minProfit, profit, p, members);
+
+        return dp[i][members][p] = (take + notTake) % MOD;
+    }
+
+public:
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n, group, 0, minProfit, profit, 0, 0);
+    }
+};
