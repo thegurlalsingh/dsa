7cf03b09ec50720747c73fa252b4c4610e79e256
thegurlalsingh <g0390076@gmail.com>
2025-11-17 14:22:12 +0530

    Time: 11 ms (12.86%) | Memory: 18.5 MB (51.59%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,43 +1,58 @@
 class Solution {
+    bool dfs(int node, vector<vector<int>>& adj, vector<int>& state) {
+        state[node] = 1;  // visiting
+        
+        for (int nxt : adj[node]) {
+            if (state[nxt] == 0) {
+                if (!dfs(nxt, adj, state)) return false;
+            }
+            else if (state[nxt] == 1) {
+                return false;   // cycle found
+            }
+        }
+        
+        state[node] = 2;  // finished
+        return true;
+    }
+
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& state, stack<int>& st){
+        state[node] = 1;  // visiting
+        
+        for (int nxt : adj[node]) {
+            if (state[nxt] == 0) {
+                dfs(nxt, adj, state, st);
+            }
+        }
+        st.push(node);
+    }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-
-        for(auto it : prerequisites){
-            int u = it[1]; // prerequisite
-            int v = it[0]; // course
-            adj[u].push_back(v);  // u → v (u must be done before v));
-        }
-
-        vector<int> indegree(numCourses, 0);
-        for(int i = 0; i < numCourses; i++){
-            for(auto it : adj[i]){
-                indegree[it]++;
+        
+        // create directed graph: a → b
+        for (auto &p : prerequisites)
+            adj[p[0]].push_back(p[1]);
+        
+        vector<int> state(numCourses, 0); // 0=unvisited,1=visiting,2=done
+        
+        // run dfs on each component
+        for (int i = 0; i < numCourses; i++) {
+            if (state[i] == 0) {
+                if (!dfs(i, adj, state)) 
+                    return {};   // cycle detected
             }
         }
-
-        queue<int> q;
-        for(int i = 0; i < numCourses; i++){
-            if(indegree[i] == 0) q.push(i);
-        }
-
-        vector<int> topo; 
-
-        while(!q.empty()){
-
-            int node = q.front();
-            topo.push_back(node);
-            q.pop();
-            
-            // reducing the indegree of adj nodes
-            for(auto it : adj[node]){
-                indegree[it]--;
-                if(indegree[it] == 0) q.push(it);
+        vector<int> state2(numCourses, 0);
+        stack<int> st; vector<int> res;
+        for (int i = 0; i < numCourses; i++) {
+            if (state2[i] == 0) {
+                dfs(i, adj, state2, st);
             }
         }
-
-        // if cycle found
-        if(topo.size() == numCourses) return topo;
-        return {}; // returning empty array if cycle not found
+        while(!st.empty()){
+            res.push_back(st.top()); st.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
