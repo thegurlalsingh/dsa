a01e0598ce2a39f50c55aeef4ae5a016dfc16df7
thegurlalsingh <g0390076@gmail.com>
2025-11-22 17:29:37 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (6.99%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
         int n = cardPoints.size();
-        int sum = 0;
-        for(int i = 0; i < n; i++) sum += cardPoints[i];
 
-        int arr = n - k;
-        if(arr == 0) return sum; // edge case: take all cards
-
-        int i = 0, j = 0;
-        int minSum = INT_MAX;
-        int s = 0;
-
-        while(j < n){
-            s += cardPoints[j];
-            if(j - i + 1 == arr){
-                minSum = min(minSum, s);
-                s -= cardPoints[i];
-                i++;
-            }
-            j++;
+        // prefix from left
+        vector<int> left(k + 1, 0);
+        for (int i = 1; i <= k; i++) {
+            left[i] = left[i - 1] + cardPoints[i - 1];
         }
 
-        return sum - minSum;
+        // prefix from right
+        vector<int> right(k + 1, 0);
+        for (int i = 1; i <= k; i++) {
+            right[i] = right[i - 1] + cardPoints[n - i];
+        }
+
+        int ans = 0;
+
+        // try all splits: i from left, k-i from right
+        for (int i = 0; i <= k; i++) {
+            ans = max(ans, left[i] + right[k - i]);
+        }
+
+        return ans;
     }
 };
