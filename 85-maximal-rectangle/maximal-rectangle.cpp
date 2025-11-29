0e44e5acc0e2282b3746c676c98cf99fc5aea30c
thegurlalsingh <g0390076@gmail.com>
2025-11-29 17:32:54 +0530

    Time: 20 ms (9.40%) | Memory: 20.6 MB (28.42%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,39 +1,50 @@
 class Solution {
 public:
-    int solve(vector<int>&arr , int prev , int cur , int val , vector<vector<int>>&dp){
-        int n = arr.size() ; 
-        if(cur>=n)
-        return 0 ; 
+    int maxRect(vector<int>& mat) {
+        int n = mat.size();
+        stack<pair<int, int>> st;
+        int maxi = 0;
 
-        if(dp[prev+1][cur] != -1)
-        return dp[prev+1][cur] ; 
-
-        val = min(val , arr[cur]) ; 
-        int ans = val*(cur-prev) ; 
-        ans = max(ans , solve(arr , prev , cur+1 , val , dp)) ; 
-        ans = max(ans , solve(arr , cur , cur+1 , INT_MAX , dp)) ;
-        return dp[prev+1][cur] = ans ; 
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && st.top().second > mat[i]) {
+                int next = i - 1;
+                int val = st.top().second;
+                st.pop();
+                int prev = st.empty() ? -1 : st.top().first;
+                maxi = max(maxi, ((next - prev) * val));
+            }
+            st.push({i, mat[i]});
+        }
+        while (!st.empty()) {
+            int next = n - 1;
+            int val = st.top().second;
+            st.pop();
+            int prev = st.empty() ? -1 : st.top().first;
+            maxi = max(maxi, ((next - prev) * val));
+        }
+        return maxi;
     }
-
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n = matrix.size() , m = matrix[0].size() ; 
-        vector<vector<int>>vtr(n , vector<int>(m , 0)) ; 
-        for(int i = 0 ; i<n ; i++){
-            for(int j = 0 ; j<m; j++){
-                if(i == 0){
-                    if(matrix[i][j] == '1')
-                    vtr[i][j] = 1 ; 
-                }else{
-                    if(matrix[i][j] == '1')
-                    vtr[i][j] = 1 + vtr[i-1][j] ; 
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> histMat(n, vector<int>(m, 0));
+        for (int i = 0; i < m; i++) {
+            histMat[0][i] = matrix[0][i] - '0';
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '0') {
+                    histMat[i][j] = 0;
+                    continue;
                 }
+                histMat[i][j] = histMat[i - 1][j] + (matrix[i][j] - '0');
             }
         }
-        int ans = 0 ; 
-        for(int i = 0 ; i<n ; i++){
-            vector<vector<int>>dp(m+1 , vector<int>(m , -1)) ; 
-            ans = max(ans , solve(vtr[i] , -1 , 0 , INT_MAX , dp)) ; 
+        int maxi = 0;
+        for (int i = 0; i < n; i++) {
+            int val = maxRect(histMat[i]);
+            maxi = max(maxi, val);
         }
-        return ans ; 
+        return maxi;
     }
 };
