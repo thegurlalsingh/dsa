e1b0241b0ac4fb7e8b32cef7320d8f56168f7d8d
thegurlalsingh <g0390076@gmail.com>
2026-01-20 21:40:03 +0530

    Time: 353 ms (29.23%) | Memory: 400.5 MB (27.69%) - LeetSync

--- 1545-form-largest-integer-with-digits-that-add-up-to-target/form-largest-integer-with-digits-that-add-up-to-target.cpp
+++ 1545-form-largest-integer-with-digits-that-add-up-to-target/form-largest-integer-with-digits-that-add-up-to-target.cpp
@@ -1,35 +1,42 @@
-class Solution
-{
-public:
-    string solve(int tgt, vector<int> &c, vector<string> &dp)
+class Solution {
+   
+    vector<string>dp;
+    string find_answer(vector<int>&cost, int target)
     {
-        if (tgt == 0)
+       
+        if(target == 0)
+        return "";
+
+        if(dp[target] != "#")
+        return dp[target];
+
+        string curr = "", answer = "0";
+        for(int i=0; i<cost.size(); i++)
         {
-            return "";
-        }
-        if (dp[tgt] != "#")
-        {
-            return dp[tgt];
-        }
-        string ans = "0";
-        for (int i = 1; i <= 9; i++)
-        {
-            if (tgt >= c[i-1])
+            if(target - cost[i] < 0)
+            continue;
+
+            curr = find_answer(cost, target - cost[i]);
+
+            if(curr == "0")
+            continue;
+
+            curr = to_string(i+1) + curr;
+
+            if(answer.size() <= curr.size())
             {
-                string curr = solve(tgt - c[i - 1], c, dp);
-                if(curr=="0")continue;
-                curr = to_string(i)+curr;
-                if(ans.length()>curr.length())continue;
-                if((ans=="0")||(curr.length()>ans.length())||(curr.length()==ans.length()&&curr.compare(ans)>0)){
-                    ans =curr;
-                }
+                answer = curr;
             }
         }
-        return dp[tgt] = ans;
+
+        return dp[target] = answer;
     }
-    string largestNumber(vector<int> &cost, int target)
-    {
-        vector<string> dp(target + 1, "#");
-        return solve(target, cost, dp);
+public:
+    string largestNumber(vector<int>& cost, int target) {
+        int n = cost.size();
+        dp.clear();
+        dp.resize(5002, "#");
+       return find_answer(cost, target);
+        
     }
 };
