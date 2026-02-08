56ab884e6ddadb027099cdcf847a44273402b979
thegurlalsingh <g0390076@gmail.com>
2026-02-08 23:02:10 +0530

    Time: 116 ms (13.02%) | Memory: 241.8 MB (5.01%) - LeetSync

--- 4018-minimum-moves-to-balance-circular-array/minimum-moves-to-balance-circular-array.cpp
+++ 4018-minimum-moves-to-balance-circular-array/minimum-moves-to-balance-circular-array.cpp
@@ -1,56 +1,37 @@
-typedef pair<int, int> P;
+typedef pair<long long, long long> P; //{move per unit, element}
 
 class Solution {
-
     struct cmp {
-        bool operator()(const P& a, const P& b) const {
-            if (a.first != b.first)
-                return a.first > b.first;   // min distance
-            return a.second < b.second;     // max balance
+        bool operator()(const P& a, const P& b) {
+            return a.first > b.first;   
         }
     };
 
 public:
     long long minMoves(vector<int>& balance) {
-        long long ans = 0;
-        long long total = accumulate(balance.begin(), balance.end(), 0LL);
-        if (total < 0) return -1;
-
-        int neg = -1;
+        int negIdx = -1; long long negElem = 0; int n = balance.size();
         for(int i = 0; i < balance.size(); i++){
             if(balance[i] < 0){
-                neg = i;
-                break;
+                negIdx = i; negElem = balance[i];
             }
         }
-
-        if(neg == -1) return 0;
-
-        int n = balance.size();
-        priority_queue<P, vector<P>, cmp> pq;
-
-        for(int i = 0; i < n; i++){
-            if(i == neg || balance[i] <= 0) continue;
-
-            int d = abs(neg - i);
-            int dist = min(d, n - d);
-            pq.push({dist, balance[i]});
-        }
-
-        while(balance[neg] < 0 && !pq.empty()){
-            auto [dist, supply] = pq.top();
-            pq.pop();
-
-            int take = min(supply, -balance[neg]);
-            ans += 1LL * take * dist;
-            balance[neg] += take;
-            supply -= take;
-
-            if(supply > 0){
-                pq.push({dist, supply});
+        long long moves = 0;
+        priority_queue<P, vector<P>, cmp> pq; 
+        for(int i = 0; i < balance.size(); i++){
+            if(i != negIdx && balance[i] > 0){
+                int idxStraight = abs(negIdx - i);
+                int idxCircular = n - abs(negIdx - i);
+                pq.push({min(idxStraight, idxCircular), balance[i]});
             }
         }
-
-        return balance[neg] == 0 ? ans : -1;
+        while(!pq.empty() && negElem < 0){
+            auto t = pq.top(); pq.pop();
+            moves += t.first * min(t.second , abs(negElem));
+            negElem += min(t.second , abs(negElem));
+        }
+        if(negElem >= 0){
+            return moves;
+        }
+        return -1;
     }
 };
