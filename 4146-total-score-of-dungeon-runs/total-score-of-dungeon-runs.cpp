ebea4f19cb1d01c6a6c32c9ffece871b9729611e
thegurlalsingh <g0390076@gmail.com>
2026-02-09 12:04:18 +0530

    Time: 42 ms (78.93%) | Memory: 259 MB (32.78%) - LeetSync

--- 4146-total-score-of-dungeon-runs/total-score-of-dungeon-runs.cpp
+++ 4146-total-score-of-dungeon-runs/total-score-of-dungeon-runs.cpp
@@ -1,26 +1,28 @@
 class Solution {
+    long long helper(vector<int>& damage, vector<int>& requirement, vector<int>& prefix, int h, int start){
+        long long count = 0;
+        long long s = requirement[start] + prefix[start] - h;
+        if(s <= 0){
+            count++;
+        }
+        auto it = lower_bound(prefix.begin(), prefix.begin() + start, s);
+        int idx = it - prefix.begin();
+        count += (start - idx);
+        return count;
+    }
 public:
     long long totalScore(int hp, vector<int>& damage, vector<int>& requirement) {
         int n = damage.size();
-        long long count = 0;
-
-        vector<long long> prefix(n);
+        vector<long long> score(n);
+        vector<int> prefix(n);
         prefix[0] = damage[0];
-        for (int i = 1; i < n; i++)
+        for(int i = 1; i < n; i++){
             prefix[i] = prefix[i - 1] + damage[i];
-
-        for (int i = 0; i < n; i++) {
-            long long s = prefix[i] + requirement[i] - hp;
-
-            if (s <= 0)
-                count++;   
-
-            auto it = lower_bound(prefix.begin(), prefix.begin() + i, s);
-            int idx = it - prefix.begin();
-
-            count += (i - idx);
         }
-
-        return count;
+        for(int i = 0; i < n; i++){
+            int temp = hp;
+            score[i] = helper(damage, requirement, prefix, temp, i);
+        }
+        return accumulate(score.begin(), score.end(), 0LL);
     }
 };
