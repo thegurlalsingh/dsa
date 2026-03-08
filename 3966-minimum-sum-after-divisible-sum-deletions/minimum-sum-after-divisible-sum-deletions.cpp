342200cd657b1228284d27f6352b403ab41ece7c
thegurlalsingh <g0390076@gmail.com>
2026-03-09 00:16:11 +0530

    Time: 327 ms (29.19%) | Memory: 328.9 MB (44.10%) - LeetSync

--- 3966-minimum-sum-after-divisible-sum-deletions/minimum-sum-after-divisible-sum-deletions.cpp
+++ 3966-minimum-sum-after-divisible-sum-deletions/minimum-sum-after-divisible-sum-deletions.cpp
@@ -1,26 +1,5 @@
 class Solution {
 public:
-    long long solve(int i, vector<int>& nums, int k, int sum,
-                    unordered_map<int,int>& mp, vector<long long>& dp) {
-
-        int n = nums.size();
-        if(i == n) return 0;
-
-        if(dp[i] != -1) return dp[i];
-
-        sum = (sum + nums[i]) % k;
-
-        long long ans = nums[i] + solve(i + 1, nums, k, sum, mp, dp);
-
-        if(mp.count(sum)) {
-            ans = min(ans, dp[mp[sum]]);
-        }
-
-        mp[sum] = i;
-
-        return dp[i] = ans;
-    }
-
     long long minArraySum(vector<int>& nums, int k) {
         int n = nums.size();
 
