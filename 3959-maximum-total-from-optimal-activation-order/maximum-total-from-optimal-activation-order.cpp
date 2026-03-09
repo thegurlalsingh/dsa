bf5f63eafc2b40620cdd91ed9e84f977ad23d2a1
thegurlalsingh <g0390076@gmail.com>
2026-03-10 02:26:57 +0530

    Time: 235 ms (53.74%) | Memory: 305.9 MB (57.82%) - LeetSync

---
+++ 3959-maximum-total-from-optimal-activation-order/maximum-total-from-optimal-activation-order.cpp
@@ -0,0 +1,1 @@
+class Solution { public: long long maxTotal(vector<int>& value, vector<int>& limit) { unordered_map<int, priority_queue<int>> umap; long long ans = 0; int n = value.size(); for (int i = 0; i < n; i++) { umap[limit[i]].push(value[i]); } for (auto &[lim, pq]: umap) { for (int i = 0; i < lim && !pq.empty(); i++) { ans += pq.top(); pq.pop(); } } return ans; } };
