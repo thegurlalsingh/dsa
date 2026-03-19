42937b8ad35ed681e6917038eb66ee5748f9c821
thegurlalsingh <g0390076@gmail.com>
2026-03-19 17:43:25 +0530

    Time: 1244 ms (14.68%) | Memory: 470.3 MB (15.38%) - LeetSync

--- 3908-minimum-time-for-k-connected-components/minimum-time-for-k-connected-components.cpp
+++ 3908-minimum-time-for-k-connected-components/minimum-time-for-k-connected-components.cpp
@@ -1,57 +1,55 @@
 class Solution {
-    void dfs(int node, vector<bool>& visited, vector<vector<pair<int, int>>>& adj, int mid) {
-        visited[node] = true;
-        for (auto& [nei, weight] : adj[node]) {
-            if (!visited[nei] && weight > mid) {
-                dfs(nei, visited, adj, mid);
-            }
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
+        vis[node] = 1;
+        for (auto it : adj[node]) {
+            if (!vis[it])
+                dfs(it, adj, vis);
         }
     }
 
-    bool check(int mid, int k, int n, vector<vector<int>>& edges, vector<vector<pair<int, int>>>& adj) {
-        vector<bool> visited(n, false);
-        int components = 0;
+    int components(int V, vector<vector<int>>& adj) {
+        vector<int> vis(V, 0);
+        int comp = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(i, visited, adj, mid);
-                components++;
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(i, adj, vis);
+                comp++;
             }
         }
+        return comp;
+    }
 
-        return components >= k;
+    vector<vector<int>> adj(int n, vector<vector<int>>& edges, int t) {
+        vector<vector<int>> a(n);
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int time = edges[i][2];
+            if (time > t) {
+                a[u].push_back(v);
+                a[v].push_back(u);
+            }
+        }
+        return a;
     }
 
 public:
     int minTime(int n, vector<vector<int>>& edges, int k) {
-        if(edges.empty()){
-            return 0;
-        }
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& e : edges) {
-            int u = e[0], v = e[1], w = e[2];
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
-        }
-        vector<int> time;
-        for(int i = 0; i < edges.size(); i++){
-            time.push_back(edges[i][2]);
-        }
-        sort(time.begin(), time.end());
         int low = 0;
-        int high = time.back();
-
-        int ans = -1;
+        int high = 1e9;
+        int ans = 0;
         while (low <= high) {
-            int mid = (low + high) / 2;
-            if (check(mid, k, n, edges, adj)) {
-                ans = mid;
-                high = mid - 1;
+            int t = (low + high) / 2;
+            vector<vector<int>> temp = adj(n, edges, t);
+            int comp = components(n, temp);
+            if (comp >= k) {
+                ans = t;
+                high = t - 1;
             } else {
-                low = mid + 1;
+                low = t + 1;
             }
         }
-
         return ans;
     }
 };
