a9b64fff7f07500d38a83ad35ee53fafb760c9da
Gurlal Singh <g0390076@gmail.com>
2026-04-11 19:58:11 +0530

    Time: 99 ms (48.46%) | Memory: 217.8 MB (42.86%) - LeetSync

--- 4089-maximize-points-after-choosing-k-tasks/maximize-points-after-choosing-k-tasks.cpp
+++ 4089-maximize-points-after-choosing-k-tasks/maximize-points-after-choosing-k-tasks.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    long long maxPoints(vector<int>& technique1, vector<int>& technique2,   int k) {
+    long long maxPoints(vector<int>& technique1, vector<int>& technique2, int k) {
         int n = (int)technique1.size();
-        vector<int> v;
-        for(int i = 0; i < n; i ++){
-            v.push_back(i);
+        vector<int> gain;
+        long long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += technique2[i];
+            gain.push_back(technique1[i] - technique2[i]);
         }
-        sort(v.begin(), v.end(), [&](const int &i, const int &j){
-            return (technique1[i] - technique2[i]) > (technique1[j] - technique2[j]);
-        });
-        long long ans = 0;
-        for(int i = 0; i < k; i ++){
-            ans += technique1[v[i]];
+
+        sort(gain.rbegin(), gain.rend());
+
+        for (int i = 0; i < k; i++)
+            sum += gain[i];
+
+        for (int i = k; i < n; i++) {
+            if (gain[i] > 0)
+                sum += gain[i];
         }
-        for(int i = k; i < n; i ++){
-            ans += max(technique1[v[i]], technique2[v[i]]);
-        }
-        return ans;
+
+        return sum;
     }
 };
