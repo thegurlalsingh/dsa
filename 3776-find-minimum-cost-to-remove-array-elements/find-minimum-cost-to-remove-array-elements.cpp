0b862511dda6dddd6d4c57103dbd51b28676a587
Gurlal Singh <g0390076@gmail.com>
2026-04-22 19:48:30 +0530

    Time: 248 ms (78.65%) | Memory: 181.9 MB (62.36%) - LeetSync

--- 3776-find-minimum-cost-to-remove-array-elements/find-minimum-cost-to-remove-array-elements.cpp
+++ 3776-find-minimum-cost-to-remove-array-elements/find-minimum-cost-to-remove-array-elements.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    int cal(int ext, int ind, vector<int>& nums, vector<vector<int>> &dp){
-        if(ind == nums.size()) {
-            return nums[ext];
+    int minCost(int prev,int i,vector<int>& nums,int remSize,vector<vector<int>>& dp){
+        if(remSize==1) return dp[prev][i] = nums[prev];
+        else if(remSize==2) return dp[prev][i] = max(nums[i],nums[prev]);
+        else{
+            if(dp[prev][i]!=-1) return dp[prev][i];
+            int pick12 = INT_MAX;
+            int pick23 = INT_MAX;
+            int pick13 = INT_MAX;
+
+            pick12 = max(nums[prev],nums[i]) + minCost(i+1,i+2,nums,remSize-2,dp);
+            pick23 = max(nums[i],nums[i+1]) + minCost(prev,i+2,nums,remSize-2,dp);
+            pick13 = max(nums[prev],nums[i+1]) + minCost(i,i+2,nums,remSize-2,dp);
+
+            return dp[prev][i] = min(pick12,min(pick23,pick13));
         }
-
-        if(ind == nums.size()-1) {
-            return max(nums[ext], nums[ind]);
-        }
-
-        if(dp[ind][ext] != -1) {
-            return dp[ind][ext];
-        }
-
-        int f = max(nums[ind], nums[ind+1]) + cal(ext, ind+2, nums, dp);
-        int s = max(nums[ext], nums[ind+1]) + cal(ind, ind+2, nums, dp);
-        int t = max(nums[ext], nums[ind]) + cal(ind+1, ind+2, nums, dp);
-
-        return dp[ind][ext] = min({f, s, t});
     }
-
     int minCost(vector<int>& nums) {
         int n = nums.size();
+        if(n==1) return nums[0];
+        else if(n==2) return max(nums[0],nums[1]);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
 
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return cal(0, 1, nums, dp);
+        int n12 = max(nums[0],nums[1]) + minCost(2,3,nums,n-2,dp);
+        int n23 = max(nums[1],nums[2]) + minCost(0,3,nums,n-2,dp);
+        int n13 = max(nums[0],nums[2]) + minCost(1,3,nums,n-2,dp);
+
+        return min(n12,min(n23,n13));
     }
 };
