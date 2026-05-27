e3b066d6e31438739fe0d363a982849fd4633519
Gurlal Singh <g0390076@gmail.com>
2026-05-27 13:10:18 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.70%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -3,30 +3,26 @@ public:
     int search(vector<int>& nums, int target) {
         int i = 0;
         int j = nums.size() - 1;
-        int index = -1;
-        while(i <= j){
-            int mid = (i + j)/2;
-            if(target == nums[mid]){
-                index = mid;
-                break;
+        int ans = -1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
             }
-            if(nums[mid] <= nums[j]){ // right part is sorted
-                if(nums[mid] < target && nums[j] >= target){
+            if (nums[i] <= nums[mid]) {
+                if (nums[i] <= target && target < nums[mid]) {
+                    j = mid - 1;
+                } else {
                     i = mid + 1;
                 }
-                else{
-                    j = mid - 1;
-                }
-            }
-            else{ // left part is sorted
-                if(nums[mid] > target && nums[i] <= target){
-                    j = mid - 1;
-                }
-                else{
+            } else {
+                if (nums[mid] < target && target <= nums[j]) {
                     i = mid + 1;
+                } else {
+                    j = mid - 1;
                 }
             }
         }
-        return index;
+        return ans;
     }
 };
