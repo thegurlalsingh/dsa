532ddcab6db3ab2dddde870728101ce4cd0bf922
Gurlal Singh <g0390076@gmail.com>
2026-05-29 00:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 77.2 MB (43.99%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,42 +1,27 @@
 class Solution {
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        while (curr) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-
 public:
+
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-
-        ListNode* t1 = l1;
-        ListNode* t2 = l2;
-
-        ListNode* res = new ListNode(-1);
-        ListNode* tail = res;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
+        ListNode* first = l1;
+        ListNode* second = l2;
         int carry = 0;
-
-        while (t1 || t2 || carry) {
-            int x = (t1 ? t1->val : 0);
-            int y = (t2 ? t2->val : 0);
-
-            int sum = x + y + carry;
+        while(first || second || carry) { //didnt focus on this carry part as while condition
+            int sum = carry;
+            if(first) {
+                sum += first->val; // missed some conditions of checking
+                first = first->next;
+            }
+            if(second) {
+                sum += second->val;
+                second = second->next;
+            }
             carry = sum / 10;
-            int digit = sum % 10;
-
-            tail->next = new ListNode(digit);
-            tail = tail->next;
-
-            if (t1) t1 = t1->next;
-            if (t2) t2 = t2->next;
+            ListNode* temp = new ListNode(sum % 10);
+            curr->next = temp;
+            curr = curr->next;
         }
-
-        // reverse again before returning
-        return res->next;
+        return dummy->next;
     }
 };
