eabcf4628c600bb825382ba5572f6516c75094f9
Gurlal Singh <g0390076@gmail.com>
2026-05-31 14:40:03 +0530

    Time: 85 ms (4.51%) | Memory: 48.3 MB (5.58%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int n = nums.size();
-        int i = 0, j = 0;
-        int sum = 0;
-        int minLen = INT_MAX;
-
-        while (j < n) {
-            sum += nums[j];
-
-            while (sum >= target) {
-                minLen = min(minLen, j - i + 1);
-                sum -= nums[i]; 
-                i++;
-            }
-
-            j++;
-        }
-
-        if(minLen == INT_MAX) {
+        if(accumulate(nums.begin(), nums.end(), 0) < target){
             return 0;
         }
-        else{
-            return minLen;
+        map<int, int> mp;
+        mp[0] = -1;
+        int prefix = 0;
+        int dis = INT_MAX;
+        for(int i = 0; i < nums.size(); i++){
+            prefix += nums[i];  
+            int diff = prefix - target;
+            auto it = mp.upper_bound(diff);
+            if(it != mp.begin()){
+                --it;
+                int t = it->second;
+                // if(t == -1){
+                //     t = 0;
+                // }
+                dis = min(dis, i - t);
+            }
+            mp[prefix] = i;
         }
+        return dis;
     }
 };
