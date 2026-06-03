fa70a6c5a1945caebe42003d8c29c08e42ff3129
Gurlal Singh <g0390076@gmail.com>
2026-06-04 00:05:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (15.78%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,51 +1,49 @@
-typedef pair<int, pair<int, int>> P; 
-// {cost, {node, stopsRemaining}}
-
 class Solution {
-    int solve(int n, vector<vector<pair<int, int>>> &adj, int src, int dest, int k) {
+    int dijkstra(int n, vector<vector<pair<int, int>>> &adj, int s, int d, int k) {
+        priority_queue<tuple<int,int, int>, vector<tuple<int,int, int>>, greater<tuple<int,int, int>>> pq;
 
-        priority_queue<P, vector<P>, greater<P>> pq;
+        vector<vector<int>> dist(n, vector<int>(k + 2, 1e9));
 
-        // dist[node][stopsLeft]
-        vector<vector<int>> dist(n, vector<int>(k+2, 1e8));
+        dist[s][k + 1] = 0;
+        pq.push({0, s, k + 1}); // taking k while doing bfs because k will change doing bfs for different nodes
 
-        dist[src][k+1] = 0;                 // we allow k stops → k+1 edges maximum
-        pq.push({0, {src, k+1}});
+        while(!pq.empty()) {
 
-        while (!pq.empty()) {
-            auto t = pq.top(); pq.pop();
-            int cost = t.first;
-            int u = t.second.first;
-            int stops = t.second.second;
+            auto [dis, node, k0] = pq.top();
+            pq.pop();
 
-            if (u == dest) return cost;     // we found the best with valid stops
+            if(node == d && k0 >= 0){
+                return dis;
+            }
+            
+            if(k0 < 0){
+                continue;
+            }
 
-            if (cost > dist[u][stops]) continue;
-            if (stops == 0) continue;
+            if(dis > dist[node][k0])
+                continue;
 
-            for (auto &p : adj[u]) {
-                int v = p.first;
-                int w = p.second;
+            for(auto &it : adj[node]) {
 
-                int newCost = cost + w;
-                if (newCost < dist[v][stops - 1]) {
-                    dist[v][stops - 1] = newCost;
-                    pq.push({newCost, {v, stops - 1}});
+                int neigh = it.first;
+                int wt    = it.second;
+
+                if(k0 - 1 >= 0 && (dist[node][k0] + wt < dist[neigh][k0 - 1])) {
+                    dist[neigh][k0 - 1] = dist[node][k0] + wt;
+                    pq.push({dist[neigh][k0 - 1], neigh, k0 - 1});
                 }
             }
         }
+
         return -1;
     }
 
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights,
-                          int src, int dst, int k) {
-
-        vector<vector<pair<int,int>>> adj(n);
-        for (auto &f : flights) {
-            adj[f[0]].push_back({f[1], f[2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        for(int i = 0; i < flights.size(); i++){
+            adj[flights[i][0]].push_back({flights[i][1], flights[i][2]});
         }
-
-        return solve(n, adj, src, dst, k);
+        return dijkstra(n, adj, src, dst, k);
     }
 };
