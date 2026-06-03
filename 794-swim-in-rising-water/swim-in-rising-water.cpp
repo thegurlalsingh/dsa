69957ec73527e85936b15a350d791d0517d8af81
Gurlal Singh <g0390076@gmail.com>
2026-06-03 18:04:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (86.30%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,42 +1,57 @@
-typedef tuple<int, int, int> P;
+typedef pair<pair<int, int>, int> P;
+vector<int> dx = {-1, 0, 1, 0};
+vector<int> dy = {0, 1, 0, -1};
+
+struct comp {
+    bool operator()(const P& a, const P& b) {
+        return a.second > b.second; // min heap on effort
+    }
+};
 
 class Solution {
-public:
-    int swimInWater(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+    int dijkstra(vector<vector<int>>& matrix) {
+        priority_queue<P, vector<P>, comp> pq;
 
-        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-        priority_queue<P, vector<P>, greater<P>> pq;
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-        int dx[4] = {-1, 0, 1, 0}; 
-        int dy[4] = {0, 1, 0, -1};
+        vector<vector<int>> dist(m, vector<int>(n, 1e9));
 
-        dist[0][0] = grid[0][0];
-        pq.push({grid[0][0], 0, 0});
+        dist[0][0] = matrix[0][0];
+        pq.push({{0, 0}, matrix[0][0]}); // remember as we are pruning early, we are returning total minimum time so push that only, not single cell weight
 
         while (!pq.empty()) {
-            auto [time, x, y] = pq.top(); 
+
+            auto t = pq.top();
+            int x = t.first.first, y = t.first.second, time = t.second;
             pq.pop();
 
-            if (x == m - 1 && y == n - 1) return time;
+            if (x == m - 1 && y == n - 1) {
+                return time;
+            }
 
-            if (time > dist[x][y]) continue;
+            if (time > dist[x][y])
+                continue;
 
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
 
-                if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
-                    int newTime = max(time, grid[nx][ny]);
-
-                    if (newTime < dist[nx][ny]) {
-                        dist[nx][ny] = newTime;
-                        pq.push({newTime, nx, ny});
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+                    int new_time = max(time, matrix[nx][ny]);
+                
+                    if (new_time < dist[nx][ny]) {
+                        dist[nx][ny] = new_time;
+                        pq.push({{nx, ny}, new_time});
                     }
                 }
             }
         }
 
         return -1;
+    }
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        return dijkstra(grid);
     }
 };
