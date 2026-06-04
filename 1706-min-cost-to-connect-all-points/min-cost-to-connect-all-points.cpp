74f3086a13671d72ece5c12ecfa40202ad56ca29
Gurlal Singh <g0390076@gmail.com>
2026-06-04 13:20:44 +0530

    Time: 922 ms (10.42%) | Memory: 242.9 MB (8.32%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,40 +1,41 @@
 class Solution {
-    int solve(vector<vector<int>>& points) {
-    int n = points.size();
-    vector<int> vis(n, 0);
-
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-
-    pq.push({0, 0}); 
-
-    int total = 0;
-
-    while (!pq.empty()) {
-        auto [wt, node] = pq.top();
-        pq.pop();
-
-        if (vis[node]) continue;
-
-        total += wt;
-
-        vis[node] = 1;
-
-        for (int v = 0; v < n; v++) {
-            if (!vis[v]) {
-                int dist = abs(points[node][0] - points[v][0]) +
-                           abs(points[node][1] - points[v][1]);
-
-                pq.push({dist, v});
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<vector<pair<int, int>>> adj(n);
+        for (int i = 0; i < points.size(); i++) {
+            int x1 = points[i][0]; int y1 = points[i][1];
+            for(int j = 0; j < points.size(); j++){
+                int x2 = points[j][0]; int y2 = points[j][1];
+                int dis = abs(x2 - x1) + abs(y2 - y1);
+                adj[i].push_back({j, dis});
+                adj[j].push_back({i, dis});
             }
         }
 
-        
-    }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-    return total;
-}
-public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        return solve(points);
+        vector<int> vis(n, 0); 
+        pq.push({0, 0});
+        int sum = 0;
+        while (!pq.empty()) {
+            auto [wt, node] = pq.top();
+            pq.pop();
+            if (vis[node]) {
+                continue;
+            }
+            vis[node] = 1;
+            sum += wt;
+            for (auto& it : adj[node]) {
+                int neigh = it.first;
+                int edgeWt = it.second;
+
+                if (!vis[neigh]) {
+                    pq.push({edgeWt, neigh});
+                }
+            }
+        }
+
+        return sum;
     }
 };
