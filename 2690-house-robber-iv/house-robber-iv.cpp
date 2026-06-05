47b1febf0b56a3e02c7ff32379ad80d8d71483fc
Gurlal Singh <g0390076@gmail.com>
2026-06-06 02:05:06 +0530

    Time: 24 ms (37.18%) | Memory: 61 MB (40.22%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,30 +1,30 @@
 class Solution {
-    bool canRob(vector<int>& nums, int k, int mid) {
-        int i = 0, count = 0;
-        while (i < nums.size()) {
-            if (nums[i] <= mid) {
+    int check(int mid, vector<int>& nums, int k){
+        int count = 0;
+        for(int i = 0; i < nums.size();){
+            if(nums[i] <= mid){
                 count++;
                 i += 2;
-            } else {
+            }
+            else{
                 i++;
             }
         }
         return count >= k;
     }
-
 public:
     int minCapability(vector<int>& nums, int k) {
-        int left = *min_element(nums.begin(), nums.end());
-        int right = *max_element(nums.begin(), nums.end());
-        int ans = right;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (canRob(nums, k, mid)) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        int low = 0;
+        int high = 1e9;
+        int ans = INT_MAX;
+        while(low <= high){
+            int mid = (low + high) / 2; // this mid is max capability of the robber and he can only steal from houses having value less than it
+            if(check(mid, nums, k)){
+                high = mid - 1; // we are trying to minimize his capability
+                ans = min(ans, mid);
+            }
+            else{
+                low = mid + 1;
             }
         }
 
