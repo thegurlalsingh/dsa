9886fbae5466eaac9ea8df5484c83eb53886ac4f
Gurlal Singh <g0390076@gmail.com>
2026-06-05 13:09:46 +0530

    Time: 673 ms (5.26%) | Memory: 55.8 MB (6.12%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,11 +1,31 @@
 class Solution {
+    bool solve(int i, vector<int>& nums, vector<int>& dp){
+        if(i >= nums.size() - 1){
+            return true;
+        }
+
+        // if(i >= n){
+        //     return false;
+        // }
+
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        
+        int ans = false;
+        for(int idx = 1; idx <= nums[i]; idx++){
+            if(i + idx < nums.size()){
+                if(solve(i + idx, nums, dp)){
+                    return dp[i] = true;
+                }
+            }
+        }
+
+        return dp[i] = false;
+    }
 public:
     bool canJump(vector<int>& nums) {
-        int maxReach = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (i > maxReach) return false;
-            maxReach = max(maxReach, i + nums[i]);
-        }
-        return true;
+        vector<int> dp(nums.size(), -1);
+        return solve(0, nums, dp);
     }
 };
