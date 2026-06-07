8219a5593fd460ea991432645a714f04aa351270
Gurlal Singh <g0390076@gmail.com>
2026-06-07 11:55:01 +0530

    Time: 51 ms (5.03%) | Memory: 151.1 MB (6.88%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+    stack<pair<int, int>> st;
+
+public:
+    MinStack() {
+        
+    }
+
+    void push(int val) {
+        if(st.empty()) {
+            st.push({val, val});
+        } else {
+            st.push({val, min(val, st.top().second)});
+        }
+    }
+
+    void pop() {
+        st.pop();
+    }
+
+    int top() {
+        return st.top().first;
+    }
+
+    int getMin() {
+        return st.top().second;
+    }
+};
