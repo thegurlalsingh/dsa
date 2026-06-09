c4f4ed271fa69d0aef8608dbdc8e3dfdf7c04661
Gurlal Singh <g0390076@gmail.com>
2026-06-09 21:23:58 +0530

    Time: 159 ms (41.01%) | Memory: 106.1 MB (30.60%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,40 +1,38 @@
 class Solution {
-    int helper(string& temp, int check){
-        if(check == 0){
-            int zeros = 0;
-            for(char c : temp) if(c == '0') zeros++;
-            return zeros;
-        } else {
-            int ones = 0;
-            for(char c : temp) if(c == '1') ones++;
-            return ones;
+    pair<int, int> helper(string &s){
+        int ones = 0, zeros = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '0'){
+                zeros++;
+            }
+            else{
+                ones++;
+            }
         }
+        return {zeros, ones};
     }
 
     int solve(int i, vector<string>& strs, int m, int n, vector<vector<vector<int>>>& dp){
-        if(i == strs.size()) return 0;
-
-        if(dp[i][m][n] != -1) return dp[i][m][n];
-
-        // Skip current string
-        int skip = solve(i + 1, strs, m, n, dp);
-
-        // Take current string if possible
-        int take = 0;
-        int t0 = helper(strs[i], 0);
-        int t1 = helper(strs[i], 1);
-        if(t0 <= m && t1 <= n){
-            take = 1 + solve(i + 1, strs, m - t0, n - t1, dp);
+        if(i >= strs.size()){
+            return 0;
         }
 
-        return dp[i][m][n] = max(take, skip);
-    }
+        if(dp[i][m][n] != -1){
+            return dp[i][m][n];
+        }
 
+        auto t = helper(strs[i]);
+        int skip = solve(i + 1, strs, m, n, dp);
+        int take = INT_MIN;
+        if(t.first <= m && t.second <= n){
+            take = 1 + solve(i + 1, strs, m - t.first, n - t.second, dp);
+        }
+
+        return dp[i][m][n] = max(skip, take);
+    }
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        int sz = strs.size();
-        // Initialize dp with -1
-        vector<vector<vector<int>>> dp(sz, vector<vector<int>>(m+1, vector<int>(n+1, -1)));
+        vector<vector<vector<int>>> dp(strs.size(), vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
         return solve(0, strs, m, n, dp);
     }
 };
