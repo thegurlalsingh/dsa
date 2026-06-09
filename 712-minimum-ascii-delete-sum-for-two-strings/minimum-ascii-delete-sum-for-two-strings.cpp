b5b17342eb005a3d18b213dbfe3b608d1c25a297
Gurlal Singh <g0390076@gmail.com>
2026-06-10 03:51:38 +0530

    Time: 35 ms (23.22%) | Memory: 18.2 MB (48.74%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,40 +1,37 @@
 class Solution {
-    string s1, s2;
-    vector<vector<int>> dp;
-
-    int solve(int i, int j) {
-        // base cases
-        if (i == s1.size() && j == s2.size()) return 0;
-
-        if (i == s1.size()) { // s1 exhausted, delete remaining s2 chars
-            int sum = 0;
-            for (int k = j; k < s2.size(); k++) sum += s2[k];
-            return sum;
+    int solve(int i, int j, string &text1, string &text2, vector<vector<int>>& dp){
+        if(i >= text1.size() || j >= text2.size()){
+            return 0;
         }
 
-        if (j == s2.size()) { // s2 exhausted, delete remaining s1 chars
-            int sum = 0;
-            for (int k = i; k < s1.size(); k++) sum += s1[k];
-            return sum;
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
 
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s1[i] == s2[j]) {
-            return dp[i][j] = solve(i + 1, j + 1);
+        int take = 0;
+        if(text1[i] == text2[j]){
+            take = (int)text1[i] + solve(i + 1, j + 1, text1, text2, dp);
         }
+        int skip = max(solve(i + 1, j, text1, text2, dp), solve(i, j + 1, text1, text2, dp));
+        
 
-        int del1 = s1[i] + solve(i + 1, j);
-        int del2 = s2[j] + solve(i, j + 1);
-
-        return dp[i][j] = min(del1, del2);
+        return dp[i][j] = max(take, skip);
     }
-
 public:
     int minimumDeleteSum(string s1, string s2) {
-        this->s1 = s1;
-        this->s2 = s2;
-        dp.assign(s1.size(), vector<int>(s2.size(), -1));
-        return solve(0, 0);
+        if(s1.size() > s2.size()){
+            swap(s2, s1);     // word1 should be bigger than word2
+        }
+        vector<vector<int>> dp(s1.size(), vector<int>(s2.size(), -1));
+        int ans = solve(0, 0, s1, s2, dp);
+        int ascii_s1 = 0;
+        int ascii_s2 = 0;
+        for(int i = 0; i < s1.size(); i++){
+            ascii_s1 += (int)s1[i];
+        }
+        for(int i = 0; i < s2.size(); i++){
+            ascii_s2 += (int)s2[i];
+        }
+        return ascii_s1 + ascii_s2 - 2 * ans;
     }
 };
