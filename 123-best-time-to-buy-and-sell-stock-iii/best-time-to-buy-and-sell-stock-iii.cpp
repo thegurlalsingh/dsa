590b267602dc070ead12fd796effbef50224b38e
Gurlal Singh <g0390076@gmail.com>
2026-06-12 13:59:01 +0530

    Time: 429 ms (31.62%) | Memory: 224 MB (16.69%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,29 +1,32 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+    int solve(int i, vector<int>& prices, vector<vector<vector<int>>>& dp,
+              int bought, int k) {
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int bought = 0; bought <= 1; bought++) {
-                for (int cap = 1; cap <= 2; cap++) {
-                    int skip = dp[i + 1][bought][cap];
-                    int buy = 0, sell = 0;
+        if (i == prices.size() || k == 0)
+            return 0;
 
-                    if (!bought)
-                        buy = -prices[i] + dp[i + 1][1][cap];
-                    else
-                        sell = prices[i] + dp[i + 1][0][cap - 1];
+        if (dp[i][bought][k] != -1)
+            return dp[i][bought][k];
 
-                    dp[i][bought][cap] = max({skip, buy, sell});
-                }
-            }
+        int ans = solve(i + 1, prices, dp, bought, k);
+
+        if (bought == 0) {
+            ans = max(ans, -prices[i] + solve(i + 1, prices, dp, 1, k));
+        } else {
+            ans = max(ans, prices[i] + solve(i + 1, prices, dp, 0, k - 1));
         }
 
-        return dp[0][0][2];
+        return dp[i][bought][k] = ans;
+    }
+
+public:
+    int maxProfit(vector<int>& prices) {
+        int k = 2;
+        int n = prices.size();
+
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
+
+        return solve(0, prices, dp, 0, k);
     }
 };
