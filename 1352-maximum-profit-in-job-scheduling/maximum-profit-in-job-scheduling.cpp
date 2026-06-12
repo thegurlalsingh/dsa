9a3070e39b1480c0da727d88d71b3e65deac49ca
Gurlal Singh <g0390076@gmail.com>
2026-06-12 18:21:49 +0530

    Time: 27 ms (86.96%) | Memory: 86.4 MB (67.62%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,34 +1,29 @@
-#include <vector>
-#include <tuple>
-#include <algorithm>
-using namespace std;
-
 class Solution {
-    vector<int> dp;
+    int solve(int i, vector<tuple<int, int, int>>& v, vector<int>& dp, int prev){
+        if(i >= v.size()){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
 
-    int solve(int i, vector<tuple<int,int,int>>& t, int n) {
-        if(i >= n) return 0;
-        if(dp[i] != -1) return dp[i];
+        int skip = solve(i + 1, v, dp, prev);
+        
+        int x = get<1>(v[i]);
+        int idx = lower_bound(v.begin(), v.end(), make_tuple(x, 0, 0)) - v.begin();
 
-        // Option 1: Skip current job
-        int notTake = solve(i + 1, t, n);
-
-        // Option 2: Take current job
-        int next = lower_bound(t.begin(), t.end(), make_tuple(get<1>(t[i]), 0, 0)) - t.begin();
-        int take = get<2>(t[i]) + solve(next, t, n);
-
-        return dp[i] = max(take, notTake);
+        int take = get<2>(v[i]) + solve(idx, v, dp, i);
+    
+        return dp[i] = max(take, skip);
     }
-
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        int n = startTime.size();
-        vector<tuple<int,int,int>> t;
-        for(int i = 0; i < n; i++) {
-            t.push_back({startTime[i], endTime[i], profit[i]});
+        vector<tuple<int, int, int>> v;
+        for(int i = 0; i < startTime.size(); i++){
+            v.push_back({startTime[i], endTime[i], profit[i]});
         }
-        sort(t.begin(), t.end());
-        dp.assign(n, -1);
-        return solve(0, t, n);
+        sort(v.begin(), v.end());
+        vector<int> dp(startTime.size(), -1);
+        return solve(0, v, dp, -1);
     }
 };
