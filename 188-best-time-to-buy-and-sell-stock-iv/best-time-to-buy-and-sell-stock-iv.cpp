3e20680229f271aae34ae80a8ed832f0dfa66f8a
Gurlal Singh <g0390076@gmail.com>
2026-06-12 13:52:52 +0530

    Time: 27 ms (13.71%) | Memory: 19.5 MB (14.00%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,29 +1,30 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
-    int solve(int i, vector<int>& prices, bool bought, int cap, vector<vector<vector<int>>>& dp) {
-        if (i == prices.size() || cap == 0) return 0;
-
-        if (dp[i][bought][cap] != -1) return dp[i][bought][cap];
-
-        int skip = solve(i + 1, prices, bought, cap, dp);
-        int buy = 0, sell = 0;
-
-        if (!bought) {
-            buy = -prices[i] + solve(i + 1, prices, true, cap, dp);
-        } else {
-            sell = prices[i] + solve(i + 1, prices, false, cap - 1, dp);
+    int solve(int i, vector<int>& prices,   vector<vector<vector<int>>>& dp, bool bought, int k){
+        if(i >= prices.size()){
+            return 0;
+        }
+        // if(k == 0){
+        //     return 0;
+        // }
+        if(dp[i][bought][k] != -1){
+            return dp[i][bought][k];
+        }
+        int skip = solve(i + 1, prices, dp, bought, k);
+        int buy = 0;
+        int sold = 0;
+        if(!bought && k > 0){
+            buy = -prices[i] + solve(i + 1, prices, dp, true, k);
+        }
+        if(bought){
+            sold = prices[i] + solve(i + 1, prices, dp, false, k - 1);
         }
 
-        return dp[i][bought][cap] = max({skip, buy, sell});
+        return dp[i][bought][k] = max({skip, buy, sold});
     }
-
 public:
     int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
-        return solve(0, prices, false, k, dp);
+        // vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(2, vector<int>(k + 1, -1)));
+        return solve(0, prices, dp, false, k);
     }
 };
