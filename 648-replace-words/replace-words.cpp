698ca3e78dfe34ba18823849e27a02ba89d863aa
Gurlal Singh <g0390076@gmail.com>
2026-06-14 17:42:15 +0530

    Time: 24 ms (90.38%) | Memory: 81 MB (27.15%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,68 +1,86 @@
-class Solution {
-public:
-    struct trieNode { 
-        trieNode *children[26]; 
-        bool isEndOfWord; 
-    };
-    
-    trieNode* getNode() {
-        trieNode* newNode = new trieNode();
-        newNode->isEndOfWord = false;
-        
-        for (int i = 0; i < 26; i++) {
-			newNode->children[i] = NULL;
-		}
-        
-        return newNode;
-    }
-
-            
-    trieNode* root;
-
-    void insert(string word) {
-        trieNode *crawler = root; 
-        
-        for (int i = 0; i < word.length(); i++) { 
-            int index = word[i] - 'a'; 
-            if (!crawler->children[index]) 
-                crawler->children[index] = getNode();
-            
-            crawler = crawler->children[index]; 
+class Trie {
+    struct Node {
+        Node* links[26];
+        bool isEnd;
+        int i;
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                links[i] = nullptr;
+            }
+            isEnd = false;
+            i = -1;
         }
-        
-        crawler->isEndOfWord = true; 
+    };
+
+    Node* root;
+
+public:
+    Trie() { root = new Node(); }
+
+    void insert(string word, int index) {
+        Node* node = root;
+        for (char ch : word) {
+            int idx = ch - 'a';
+            if (node->links[idx] == nullptr) {
+                node->links[idx] = new Node();
+            }
+            node = node->links[idx];
+        }
+        node->isEnd = true;
+        node->i = index;
     }
-    
-    string search(string word) {
-        trieNode *crawler = root; 
-        
-        for (int i = 0; i < word.length(); i++) { 
-            int index = word[i] - 'a'; 
-            if (!crawler->children[index]) 
-                return word; 
-            crawler = crawler->children[index];
-            if(crawler->isEndOfWord) {
-                return word.substr(0, i+1);
+
+    Node* startsWith(string prefix) {
+        Node* node = root;
+        string ans = "";
+        for(char ch : prefix) {
+            int idx = ch - 'a';
+            if(node->links[idx] == NULL)
+                return new Node();
+            ans += ch;
+            node = node->links[idx];
+            if(node->isEnd){
+                return node;
             }
         }
-        return word;
-    }
 
+        return node;
+    }
+};
+
+class Solution {
+public:
     string replaceWords(vector<string>& dictionary, string sentence) {
+        Trie t;
+
+        for(int i = 0; i < dictionary.size(); i++){
+            t.insert(dictionary[i], i);
+        }
 
         stringstream ss(sentence);
+
         string word;
-        string result;
-        root = getNode();
-        for (string word : dictionary) {
-            insert(word);
+
+        vector<string> v;
+
+        while(ss >> word){
+            v.push_back(word);
         }
 
-        while(getline(ss, word, ' ')) {
-            result += search(word) + " ";
+        string k = "";
+
+        for(int i = 0; i < v.size(); i++){
+            int index = t.startsWith(v[i])->i;
+            if(index != -1){
+                v[i] = dictionary[index];
+            }
+
+            k += v[i];
+            if(i != v.size() - 1){
+                k += ' ';
+            }
         }
 
-        result.pop_back();
-        return result;
+        return k;
     }
 };
