d805abd1a5cd60d539ecb25bedb9846a1e66002d
Gurlal Singh <g0390076@gmail.com>
2026-06-14 12:28:58 +0530

    Time: 9 ms (74.09%) | Memory: 12.6 MB (77.81%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,53 +1,41 @@
 class Solution {
-private:
-    int findSubset(int idx, vector<int>& arr, vector<int>& dp, vector<int>& hash) {
-    if (dp[idx] != -1) {
-        return dp[idx]; 
-    }
-
-    int maxLength = 1; 
-    hash[idx] = idx;   
-
-    for (int prev_index = 0; prev_index < idx; prev_index++) {
-        if (arr[idx] % arr[prev_index] == 0) {
-            int currLength = 1 + findSubset(prev_index, arr, dp, hash);
-            if (currLength > maxLength) {
-                maxLength = currLength;
-                hash[idx] = prev_index;
-            }
-        }
-    }
-
-    return dp[idx] = maxLength; 
-}
-
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n = nums.size();
 
         sort(nums.begin(), nums.end());
 
-        vector<int> dp(n, -1);   
-        vector<int> hash(n);     
+        vector<int> dp(n, 1);
+        vector<int> parent(n, -1);
 
-        int ans = 1, lastIndex = 0;
+        int maxLen = 1;
+        int lastIdx = 0;
+
         for (int i = 0; i < n; i++) {
-            int length = findSubset(i, nums, dp, hash);
-            if (length > ans) {
-                ans = length;
-                lastIndex = i;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        parent[i] = j;
+                    }
+                }
+            }
+
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                lastIdx = i;
             }
         }
 
-        vector<int> temp;
-        temp.push_back(nums[lastIndex]);
-        while (hash[lastIndex] != lastIndex) {
-            lastIndex = hash[lastIndex];
-            temp.push_back(nums[lastIndex]);
+        vector<int> ans;
+
+        while (lastIdx != -1) {
+            ans.push_back(nums[lastIdx]);
+            lastIdx = parent[lastIdx];
         }
 
-        reverse(temp.begin(), temp.end());
+        reverse(ans.begin(), ans.end());
 
-        return temp;
+        return ans;
     }
 };
