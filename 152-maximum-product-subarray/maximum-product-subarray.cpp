0899bd908ca64c0196dc1aadb4512f49ba1ab4b1
Gurlal Singh <g0390076@gmail.com>
2026-06-30 08:31:57 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (5.11%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
-    pair<int, int> helper(vector<int>& nums, int i, int& ans) {
-    if (i == 0) {
-        ans = max(ans, nums[0]);
-        return {nums[0], nums[0]}; // {maxProd, minProd} at index 0
+    int maxProduct(vector<int>& nums) {
+        vector<int> maxi(nums.size(), 0);
+        vector<int> mini(nums.size(), 0);
+        maxi[0] = nums[0];
+        mini[0] = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            maxi[i] = max({nums[i], nums[i] * maxi[i - 1], nums[i] * mini[i - 1]});
+            mini[i] = min({nums[i], nums[i] * maxi[i - 1], nums[i] * mini[i - 1]});
+        }
+        int m_maxi = *max_element(maxi.begin(), maxi.end());
+        int m_mini = *max_element(mini.begin(), mini.end());
+        return max(m_maxi, m_mini);
     }
-
-    auto [prevMax, prevMin] = helper(nums, i - 1, ans);
-
-    int cur1 = nums[i];
-    int cur2 = nums[i] * prevMax;
-    int cur3 = nums[i] * prevMin;
-
-    int curMax = max({cur1, cur2, cur3});
-    int curMin = min({cur1, cur2, cur3});
-
-    ans = max(ans, curMax);
-
-    return {curMax, curMin};
-}
-
-int maxProduct(vector<int>& nums) {
-    int ans = INT_MIN;
-    helper(nums, nums.size() - 1, ans);
-    return ans;
-}
-
 };
