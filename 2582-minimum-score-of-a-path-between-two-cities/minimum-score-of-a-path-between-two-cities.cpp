2770867f4f17219c7b2bd86f7f9a8c75149a7a99
Gurlal Singh <g0390076@gmail.com>
2026-07-04 12:32:55 +0530

    Time: 83 ms (48.62%) | Memory: 145.8 MB (22.19%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,6 +1,7 @@
 class DisjointSet {
-    vector<int> rank, parent;
+
 public:
+    vector<int> rank, parent;
     DisjointSet(int n) {
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
@@ -18,34 +19,57 @@ public:
     void unionByRank(int u, int v) {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
+        if (ulp_u == ulp_v)
+            return;
         if (rank[ulp_u] < rank[ulp_v]) {
             parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u]) {
+        } else if (rank[ulp_v] < rank[ulp_u]) {
             parent[ulp_v] = ulp_u;
-        }
-        else {
+        } else {
             parent[ulp_v] = ulp_u;
             rank[ulp_u]++;
         }
     }
 };
+
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        DisjointSet ds(n);
-        for(int i = 0; i < roads.size(); i++){
-            ds.unionByRank(roads[i][0], roads[i][1]);
+        DisjointSet dsu(n);
+        unordered_set<int> s;
+        for (int i = 0; i < roads.size(); i++) {
+            dsu.unionByRank(roads[i][0] - 1, roads[i][1] - 1);
         }
-        sort(roads.begin(), roads.end(), [](vector<int>& a, vector<int>& b){
-            return a[2] < b[2];
-        });
-        for(int i = 0; i < roads.size(); i++){
-            if(ds.findUPar(n) == ds.findUPar(roads[i][0])){
-                return roads[i][2];
+        vector<int> par = dsu.parent;
+        for (int i = 1; i < n; i++) {
+            if (dsu.findUPar(i) == dsu.findUPar(0)) {
+                s.insert(i);
             }
         }
-        return -1;
+        vector<vector<pair<int, int>>> adj(n);
+        for (int i = 0; i < roads.size(); i++) {
+            int u = roads[i][0] - 1;
+            int v = roads[i][1] - 1;
+            int w = roads[i][2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+        queue<int> q;
+        q.push(0);
+
+        int best = INT_MAX;
+        while (!q.empty()) {
+            auto t = q.front();
+            q.pop();
+            for (auto& [neigh, wt] : adj[t]) {
+                best = min(wt, best);
+                if (s.find(neigh) != s.end()) {
+                    s.erase(neigh);
+                    q.push(neigh);
+                }
+                
+            }
+        }
+        return best;
     }
 };
