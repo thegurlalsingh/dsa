e4472b9e63ad24e4be5bd4b0e7cb615567f5b6ae
Gurlal Singh <g0390076@gmail.com>
2026-07-06 18:26:09 +0530

    Time: 31 ms (30.87%) | Memory: 254 MB (28.57%) - LeetSync

--- 4062-house-robber-v/house-robber-v.cpp
+++ 4062-house-robber-v/house-robber-v.cpp
@@ -1,28 +1,26 @@
 class Solution {
-    long long solve(int i, bool prevTaken, vector<int>& nums, vector<int>& colors, vector<vector<long long>>& dp) {
-
-        if(i >= nums.size()) {
+    long long solve(int i, vector<int>& nums, vector<int>& colors, vector<long long>& dp){
+        if(i >= nums.size()){
             return 0;
         }
-
-        if(dp[i][prevTaken] != -1) {
-            return dp[i][prevTaken];
+        if(dp[i] != -1){
+            return dp[i];
         }
 
-        long long way1 = solve(i + 1, false, nums, colors, dp);
-
-        long long way2 = 0;
-        if(i == 0 || !prevTaken || colors[i] != colors[i - 1]) {
-            way2 = nums[i] + solve(i + 1, true, nums, colors, dp);
+        long long skip = solve(i + 1, nums, colors, dp);
+        long long take = 0;
+        if(i + 1 < nums.size() && colors[i] == colors[i + 1]){
+            take = nums[i] + solve(i + 2, nums, colors, dp);
+        }
+        else{
+            take = nums[i] + solve(i + 1, nums, colors, dp);
         }
 
-        return dp[i][prevTaken] = max(way1, way2);
+        return dp[i] = max(take, skip);
     }
-
 public:
     long long rob(vector<int>& nums, vector<int>& colors) {
-        int n = nums.size();
-        vector<vector<long long>> dp(n, vector<long long>(2, -1));
-        return solve(0, false, nums, colors, dp);
+        vector<long long> dp(nums.size(), -1);
+        return solve(0, nums, colors, dp);
     }
 };
