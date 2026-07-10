5d8eafc28ae9c23523117af4c59ead483fbc5682
Gurlal Singh <g0390076@gmail.com>
2026-07-10 18:52:20 +0530

    Time: 6 ms (50.15%) | Memory: 19.4 MB (55.54%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> inorder(numCourses, 0);
         vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
-
-        for(int i = 0; i < prerequisites.size(); i++){
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            indegree[v]++;
-            adj[u].push_back(v);
-        }
-
         queue<int> q;
 
-        for(int i = 0; i < indegree.size(); i++){
-            if(indegree[i] == 0){
+        for(int i = 0; i < prerequisites.size(); i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            inorder[prerequisites[i][0]]++;
+        }
+
+        for(int i = 0; i < inorder.size(); i++){
+            if(inorder[i] == 0){
                 q.push(i);
             }
         }
 
         vector<int> ans;
+
         while(!q.empty()){
-            int t = q.front(); q.pop();
+            auto t = q.front(); q.pop();
             ans.push_back(t);
             for(int neigh : adj[t]){
-                indegree[neigh]--;
-                if(indegree[neigh] == 0){
+                inorder[neigh]--;
+                if(inorder[neigh] == 0){
                     q.push(neigh);
                 }
             }
         }
 
-        return ans.size() == numCourses ? true : false;
+        if(numCourses == ans.size()){
+            return true;
+        }
+        return false;
     }
 };
