0fac34362ee40ccf1795a77a18ac7917633d4917
Gurlal Singh <g0390076@gmail.com>
2026-07-29 07:43:34 +0530

    Time: 199 ms (36.62%) | Memory: 260.6 MB (87.72%) - LeetSync

--- 3804-maximize-active-section-with-trade-ii/maximize-active-section-with-trade-ii.cpp
+++ 3804-maximize-active-section-with-trade-ii/maximize-active-section-with-trade-ii.cpp
@@ -1,110 +1,142 @@
-struct Tree {
+class SegmentTree {
     int n;
-    vector<vector<int>> lidx, ridx;
-    vector<int> segtree;
-    string t;
-    Tree(string &s) {
-        n = s.size();
-        t = s;
-        segtree.resize(4*n);
-        lidx.assign(2, vector<int>(n));
-        ridx.resize(2, vector<int>(n));
-        for (int i = 0; i < n; i++) {
-            int x = s[i] - '0';
-            lidx[x][i] = (i == 0 || (s[i] != s[i - 1]) ? i : lidx[x][i - 1]);
+    vector<int> tree;
+
+    void build(int node, int l, int r, vector<int> &a) {
+        if (l == r) {
+            tree[node] = a[l];
+            return;
         }
-        for (int i = n - 1; i >= 0; i--) {
-            int x = s[i] - '0';
-            ridx[x][i] = (i == n - 1 || (s[i] != s[i + 1]) ? i : ridx[x][i + 1]);
-        }
-        build(1, 0, n - 1);
+
+        int mid = (l + r) >> 1;
+
+        build(node << 1, l, mid, a);
+        build(node << 1 | 1, mid + 1, r, a);
+
+        tree[node] = max(tree[node << 1], tree[node << 1 | 1]);
     }
-    int merge(int lseg, int rseg, int s, int m, int e) {
-        if (s == e) return 0;
-        int res = max(lseg, rseg);
-        if (t[m] == t[m + 1]) {
-            if (t[m] == '1') {
-                if (lidx[1][m] - 1 >= s && ridx[1][m] + 1 <= e) {
-                    int L = max(s, lidx[0][lidx[1][m] - 1]);
-                    int R = min(e, ridx[0][ridx[1][m] + 1]);
-                    res = max(res, R - L + 1 - (ridx[1][m] - lidx[1][m] + 1));
-                }
-            } else {
-                {
-                    int idx1 = lidx[0][m] - 1;
-                    if (idx1 >= s) {
-                        int idx0 = lidx[1][idx1] - 1;
-                        if (idx0 >= s) {
-                            int L = max(s, lidx[0][idx0]);
-                            int R = min(e, ridx[0][m]);
-                            res = max(res, R - L + 1 - (ridx[1][idx1] - lidx[1][idx1] + 1));
-                        }
-                    }
-                }
-                
-                {
-                    int idx1 = ridx[0][m] + 1;
-                    if (idx1 <= e) {
-                        int idx0 = ridx[1][idx1] + 1;
-                        if (idx0 <= e) {
-                            int L = max(s, lidx[0][m]);
-                            int R = min(e, ridx[0][idx0]);
-                            res = max(res, R - L + 1 - (ridx[1][idx1] - lidx[1][idx1] + 1));
-                        }
-                    }
-                }
-            }
-        } else {  
-            if (t[m] == '1') {
-                if (lidx[1][m] - 1 >= s) {
-                    int L = max(s, lidx[0][lidx[1][m] - 1]);
-                    int R = min(e, ridx[0][m + 1]);
-                    res = max(res, R - L + 1 - (ridx[1][m] - lidx[1][m] + 1));
-                }
-            } 
-            else {
-                if (ridx[1][m + 1] + 1 <= e) {
-                    int L = max(s, lidx[0][m]);
-                    int R = min(e, ridx[0][ridx[1][m + 1] + 1]);
-                    res = max(res, R - L + 1 - (ridx[1][m + 1] - lidx[1][m + 1] + 1));
-                }
-            }
-        }
-        return res;
-    }
-    void build(int idx, int s, int e) {
-        if (s == e) return;
-        int m = (s + e)/2;
-        build(2*idx, s, m);
-        build(2*idx + 1, m + 1, e);
-        segtree[idx] = merge(segtree[2*idx], segtree[2*idx + 1], s, m, e);
-    }
-    int query(int idx, int s, int e, int l, int r) {
-        if (s > r || l > e || s > e || l > r) return -1;
-        if (l <= s && e <= r) return segtree[idx];
-        int m = (s + e)/2;
-        int lans = query(2*idx, s, m, l, r);
-        if (lans == -1) return query(2*idx + 1, m + 1, e, l, r);
-        int rans = query(2*idx + 1, m + 1, e, l, r);
-        if (rans == -1) return lans;
-        return merge(lans, rans, max(s, l), m, min(e, r));
-    }
-};
-class Solution {
-public:
-    vector<int> maxActiveSectionsAfterTrade(string s, vector<vector<int>>& queries) {
-        Tree tree(s);
-        int m = queries.size();
-        int n = s.size();
-        vector<int> ans(m);
-        int cnt = 0;
-        for (int i = 0; i < n; i++) if (s[i] == '1') cnt++;
-        for (int i = 0; i < m; i++) {
-            int l = queries[i][0];
-            int r = queries[i][1];
-            ans[i] = cnt + tree.query(1, 0, n - 1, l, r);
-        }
+
+    int query(int node, int l, int r, int ql, int qr) {
+
+        if (ql <= l && r <= qr)
+            return tree[node];
+
+        int mid = (l + r) >> 1;
+        int ans = 0;
+
+        if (ql <= mid)
+            ans = max(ans, query(node << 1, l, mid, ql, qr));
+
+        if (qr > mid)
+            ans = max(ans, query(node << 1 | 1, mid + 1, r, ql, qr));
+
         return ans;
+    }
+
+public:
+
+    SegmentTree(vector<int> &a) {
+
+        n = a.size();
+        tree.assign(4 * max(1, n), 0);
+
+        if (n)
+            build(1, 0, n - 1, a);
+    }
+
+    int query(int l, int r) {
+
+        if (l > r || n == 0)
+            return 0;
+
+        return query(1, 0, n - 1, l, r);
     }
 };
 
+class Solution {
+public:
+    vector<int> maxActiveSectionsAfterTrade(string s,
+                                            vector<vector<int>>& queries) {
+
+        int totalOne = count(s.begin(), s.end(), '1');
+
+        vector<int> zeroLen;
+        vector<int> left;
+        vector<int> right;
+
+        int n = s.size();
+
+        for (int i = 0; i < n;) {
+
+            int j = i;
+
+            while (j < n && s[j] == s[i])
+                j++;
+
+            if (s[i] == '0') {
+                zeroLen.push_back(j - i);
+                left.push_back(i);
+                right.push_back(j - 1);
+            }
+
+            i = j;
+        }
+
+        int m = zeroLen.size();
+
+        if (m < 2)
+            return vector<int>(queries.size(), totalOne);
+
+        vector<int> gain(m - 1);
+
+        for (int i = 0; i < m - 1; i++)
+            gain[i] = zeroLen[i] + zeroLen[i + 1];
+
+        SegmentTree st(gain);
+
+        vector<int> ans;
+
+        for (auto &q : queries) {
+
+            int l = q[0];
+            int r = q[1];
+
+            int first =
+                lower_bound(right.begin(), right.end(), l) - right.begin();
+
+            int last =
+                upper_bound(left.begin(), left.end(), r) - left.begin() - 1;
+
+            if (first >= last || first >= m || last < 0) {
+                ans.push_back(totalOne);
+                continue;
+            }
+
+            int firstLen =
+                right[first] - max(left[first], l) + 1;
+
+            int lastLen =
+                min(right[last], r) - left[last] + 1;
+
+            if (first + 1 == last) {
+                ans.push_back(totalOne + firstLen + lastLen);
+                continue;
+            }
+
+            int best = 0;
+
+            // First partial zero block.
+            best = max(best, firstLen + zeroLen[first + 1]);
+
+            // Last partial zero block.
+            best = max(best, zeroLen[last - 1] + lastLen);
+
+            // Completely inside the substring.
+            best = max(best, st.query(first + 1, last - 2));
+
+            ans.push_back(totalOne + best);
+        }
+
+        return ans;
+    }
+};
