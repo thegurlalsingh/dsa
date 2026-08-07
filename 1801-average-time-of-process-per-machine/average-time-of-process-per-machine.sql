8864904b637f6fa944bb523787635f404ae569c9
Gurlal Singh <g0390076@gmail.com>
2026-08-07 21:22:34 +0530

    Time: 267 ms (48.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,15 @@
+WITH temp AS (
+    SELECT
+        machine_id,
+        process_id,
+        MAX(CASE WHEN activity_type = 'start' THEN timestamp END) AS start_time,
+        MAX(CASE WHEN activity_type = 'end' THEN timestamp END) AS end_time
+    FROM Activity
+    GROUP BY machine_id, process_id
+)
+
+SELECT
+    machine_id,
+    ROUND(AVG(end_time - start_time), 3) AS processing_time
+FROM temp
+GROUP BY machine_id;
