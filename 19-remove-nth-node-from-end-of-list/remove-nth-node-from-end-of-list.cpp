f6c9b3b7145c50091f797fa36120817407fbc5ce
Gurlal Singh <g0390076@gmail.com>
2026-08-19 20:45:08 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (93.65%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -9,31 +9,33 @@
  * };
  */
 class Solution {
+    int size(ListNode* head){
+        int t = 0;
+        ListNode* curr = head;
+        while(curr){
+            t++;
+            curr = curr->next;
+        }
+        return t;
+    }
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head->next == nullptr){
+        int sz = size(head);
+        if(sz == 1){
             return nullptr;
         }
-        
-        int count = 0;
-        ListNode* temp = head;
-        while(temp){
-            count++;
-            temp = temp->next;
-        }
-        if(count == n) {
+        ListNode* curr = head;
+        if(sz - n == 0){
             return head->next;
         }
-        int prev = count - n - 1;
-        temp = head;
-        while(prev--){
-            temp = temp->next;
+        int r = sz - n - 1;
+        
+        while(curr && r--){
+            curr = curr->next;
         }
-        ListNode* nex = nullptr;
-        if(temp->next->next){
-            nex = temp->next->next;
-        }
-        temp->next = nex;
+
+        ListNode* ne = curr->next->next;
+        curr->next = ne;
         return head;
     }
 };
