9d80f9546fe2c7ea15bccd4b2ace36fa1cc3b53d
Gurlal Singh <g0390076@gmail.com>
2026-08-21 20:23:29 +0530

    Time: 2 ms (38.97%) | Memory: 77.2 MB (46.51%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,27 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
-public:
+    ListNode* reverse(ListNode* h){
+        ListNode* prev = nullptr;
+        ListNode* curr = h;
 
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* curr = dummy;
-        ListNode* first = l1;
-        ListNode* second = l2;
-        int carry = 0;
-        while(first || second || carry) { //didnt focus on this carry part as while condition
-            int sum = carry;
-            if(first) {
-                sum += first->val; // missed some conditions of checking
-                first = first->next;
-            }
-            if(second) {
-                sum += second->val;
-                second = second->next;
-            }
-            carry = sum / 10;
-            ListNode* temp = new ListNode(sum % 10);
-            curr->next = temp;
-            curr = curr->next;
+        while(curr){
+            ListNode* n = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = n;
         }
+
+        return prev;
+
+    }
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* revl1 = l1;
+        ListNode* revl2 = l2;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr1 = revl1;
+        ListNode* curr2 = revl2;
+        ListNode* curr3 = dummy;
+        int carry = 0;
+
+        while(curr1 || curr2 || carry){
+            int t = carry;
+            if (curr1) {
+                t += curr1->val;
+            }
+            if (curr2) {
+                t += curr2->val;
+            }
+            int add = t % 10; int rem = t / 10;
+            ListNode* n = new ListNode(add);
+            carry = rem;
+            curr3->next = n;
+            curr3 = curr3->next;
+            if(curr1){
+                curr1 = curr1->next;
+            }
+            if(curr2){
+                curr2 = curr2->next;
+            }
+        }
+
         return dummy->next;
     }
 };
