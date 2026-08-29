8fc6e7a5ffb06505640ff2321bd09b7fc6d3616c
Gurlal Singh <g0390076@gmail.com>
2026-08-29 20:28:33 +0530

    Time: 197 ms (10.06%) | Memory: 23 MB (5.02%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,15 +1,21 @@
 class Solution {
-    int solve(int i, vector<int>& nums, vector<int>& dp){
-        if(i >= nums.size() - 1){
+    int solve(int i, vector<int>& nums, vector<int>& dp) {
+        if(i >= nums.size())
+            return INT_MAX;
+
+        if(i == nums.size() - 1)
             return 0;
-        }
-        if(dp[i] != -1){
+
+        if(dp[i] != -1)
             return dp[i];
+
+        int ans = INT_MAX;
+        for(int j = 1; j <= nums[i]; j++) {
+            int t = solve(i + j, nums, dp);
+            if(t != INT_MAX)
+                ans = min(ans, 1 + t);
         }
-        int ans = 1e9;
-        for(int end = 1; end < nums.size() && end <= nums[i]; end++){
-            ans = min(ans, 1 + solve(i + end, nums, dp));
-        }
+
         return dp[i] = ans;
     }
 public:
