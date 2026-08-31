f4a17ab914e322bec99f6f587bed65f2f15c6ad3
Gurlal Singh <g0390076@gmail.com>
2026-08-31 21:08:34 +0530

    Time: 3 ms (12.55%) | Memory: 18.5 MB (14.01%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m = image.size(); int n = image[0].size();
-        vector<int> dr = {-1, 0, 1, 0};
-        vector<int> dc = {0, 1, 0, -1};
-        vector<vector<int>> ne = image;
-        queue<tuple<int, int, int>> q;
-        int inc = ne[sr][sc];
-        q.push({sr, sc, inc});
-        if(inc == color){
-            return ne;
-        }
-        while (!q.empty()) {
-            auto t = q.front();
+        vector<pair<int, int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        queue<pair<int, int>> q;
+        vector<vector<int>> ans = image;
+        vis[sr][sc] = 1;
+        int source = image[sr][sc];
+        ans[sr][sc] = color;
+        q.push({sr, sc});
+        while(!q.empty()){
+            auto [r, c] = q.front();
             q.pop();
-            // if(get<2>(t) == color){
-            //     continue;
-            // }
-            ne[get<0>(t)][get<1>(t)] = color;
-            for (int k = 0; k < 4; k++) {
-                int nr = get<0>(t) + dr[k];
-                int nc = get<1>(t) + dc[k];
+            
+            for (auto [dr, dc] : directions) {
+                int nr = r + dr;
+                int nc = c + dc;
 
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
-                    if(ne[nr][nc] == inc){
-                        ne[nr][nc] = color;
-                        q.push({nr, nc, inc});
-                    }
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && image[nr][nc] == source && !vis[nr][nc]) {
+                    vis[nr][nc] = 1;
+                    ans[nr][nc] = color;
+                    q.push({nr, nc});
                     
                 }
-                // q.push({nr, nc, color});
             }
         }
-        return ne;
+
+        return ans;
     }
 };
