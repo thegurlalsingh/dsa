5ed0e002d3fed1406fec501ee98d632ffbfa3ac1
Gurlal Singh <g0390076@gmail.com>
2026-08-31 19:08:10 +0530

    Time: 5 ms (15.74%) | Memory: 21.9 MB (5.51%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,55 +1,42 @@
-class DisjointSet {
-
-public:
-    vector<int> rank, parent;
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
-    }
-
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v)
-            return;
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-        } else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
-        } else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-};
-
 class Solution {
-public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        DisjointSet ds(isConnected.size());
-        int provinces = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (isConnected[i][j] == 1) {
-                    ds.unionByRank(i, j);
+    void bfs(vector<vector<int>>& adj, int node, vector<int>& vis){
+        int n = vis.size();
+        queue<int> q;
+        q.push(node);
+        while(!q.empty()){
+            auto x = q.front(); q.pop();
+            if(vis[x]){
+                continue;
+            }
+            vis[x] = 1;
+            for (auto neigh : adj[x]) {
+                if (!vis[neigh]) {
+                    q.push(neigh);
                 }
             }
         }
-        unordered_map<int, int> mp;
-        for (int i = 0; i < isConnected.size(); i++) {
-            if (ds.findUPar(i) == i)
-                provinces++;
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size(); 
+        int count = 0;
+        vector<vector<int>> adj(n);
+        vector<int> vis(n, 0);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(isConnected[i][j]){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
         }
-        return provinces;
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+                count++;
+                bfs(adj, i, vis);
+            }
+        }
+
+        return count;
     }
 };
