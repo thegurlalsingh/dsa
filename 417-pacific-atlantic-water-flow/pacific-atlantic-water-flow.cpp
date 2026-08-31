d291bb2c60dd945d2ca5f96eddc96f07e94ead2e
Gurlal Singh <g0390076@gmail.com>
2026-08-31 17:06:46 +0530

    Time: 1 ms (92.24%) | Memory: 25.1 MB (20.36%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,84 +1,63 @@
 class Solution {
-    bool isValid(int r, int c, int m, int n){
-        return r >= 0 && c >= 0 && r < m && c < n;
-    }
-
-    struct PairHash {
-        size_t operator()(const pair<int,int>& p) const {
-            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
-        }
+    vector<pair<int, int>> directions = {
+        {-1, 0}, 
+        {0, 1},  
+        {1, 0},  
+        {0, -1}  
     };
+
+    void solve(queue<pair<int, int>> q, vector<vector<int>>& vis, vector<vector<int>>& heights) {
+        int m = heights.size();
+        int n = heights[0].size();
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+            if (vis[r][c]) {
+                continue;
+            }
+            vis[r][c] = 1;
+            for (auto [dr, dc] : directions) {
+                int nr = r + dr;
+                int nc = c + dc;
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && (heights[nr][nc] >= heights[r][c])) {
+                    q.push({nr, nc});
+                }
+            }
+        }
+    }
 
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int m = heights.size(); int n = heights[0].size();
-        unordered_set<pair<int, int>, PairHash> pacific, atlantic;
-        vector<vector<int>> visited1(m, vector<int>(n, 0));
-        vector<vector<int>> visited2(m, vector<int>(n, 0));
+        int m = heights.size();
+        int n = heights[0].size();
+        vector<vector<int>> pacificvis(m, vector<int>(n, 0));
+        vector<vector<int>> atlanticvis(m, vector<int>(n, 0));
+        queue<pair<int, int>> pacificq, atlanticq;
 
-        for(int i = 0; i < n; i++){
-            pacific.insert({0, i}); // top -> pacific
-            visited1[0][i] = 1;
+        for (int i = 0; i < m; i++) {
+            pacificq.push({i, 0});
         }
-        for(int i = 0; i < n; i++){
-            atlantic.insert({m - 1, i}); // bottom -> atlantic
-            visited2[m - 1][i] = 1;
+        for (int i = 0; i < m; i++) {
+            atlanticq.push({i, n - 1});
         }
-        for(int i = 0; i < m; i++){
-            pacific.insert({i, 0}); // left -> pacific
-            visited1[i][0] = 1;
+        for (int i = 0; i < n; i++) {
+            pacificq.push({0, i});
         }
-        for(int i = 0; i < m; i++){
-            atlantic.insert({i, n - 1}); // right -> atlantic
-            visited2[i][n - 1] = 1;
+        for (int i = 0; i < n; i++) {
+            atlanticq.push({m - 1, i});
         }
 
-        int dx[] = {-1, 0, 1, 0};
-        int dy[] = {0, 1, 0, -1};
+        solve(pacificq, pacificvis, heights);
+        solve(atlanticq, atlanticvis, heights);
 
-        queue<pair<int, int>> q;
-        for(auto p : pacific){
-            q.push(p); // multi source bfs
-        }
-
-        while(!q.empty()){
-            auto [r, c] = q.front();
-            q.pop();
-
-            for(int k = 0; k < 4; k++){
-                int nr = r + dx[k];
-                int nc = c + dy[k];
-                if(isValid(nr, nc, m, n) && !visited1[nr][nc] && (heights[nr][nc] >= heights[r][c])){
-                    q.push({nr, nc});
-                    visited1[nr][nc] = 1;
-                    pacific.insert({nr, nc});
-                }
-            }
-        }
-
-        for(auto p : atlantic){
-            q.push(p); // multi source bfs
-        }
-
-        while(!q.empty()){
-            auto [r, c] = q.front();
-            q.pop();
-
-            for(int k = 0; k < 4; k++){
-                int nr = r + dx[k];
-                int nc = c + dy[k];
-                if(isValid(nr, nc, m, n) && !visited2[nr][nc] && (heights[nr][nc] >= heights[r][c])){
-                    q.push({nr, nc});
-                    visited2[nr][nc] = 1;
-                    atlantic.insert({nr, nc});
-                }
-            }
-        }
-        
         vector<vector<int>> ans;
-        for(auto& p : pacific){
-            if(atlantic.find(p) != atlantic.end()){
-                ans.push_back({p.first, p.second});
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacificvis[i][j] && atlanticvis[i][j]) {
+                    ans.push_back({i, j});
+                }
             }
         }
 
