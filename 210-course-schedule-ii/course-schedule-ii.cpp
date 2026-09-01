5a9410974f2d90d2ef2ff998a354e105b1847d8e
Gurlal Singh <g0390076@gmail.com>
2026-09-01 14:40:31 +0530

    Time: 2 ms (76.11%) | Memory: 18.1 MB (83.00%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,58 +1,35 @@
 class Solution {
-    bool dfs(int node, vector<vector<int>>& adj, vector<int>& state) {
-        state[node] = 1;  // visiting
-        
-        for (int nxt : adj[node]) {
-            if (state[nxt] == 0) {
-                if (!dfs(nxt, adj, state)) return false;
-            }
-            else if (state[nxt] == 1) {
-                return false;   // cycle found
-            }
-        }
-        
-        state[node] = 2;  // finished
-        return true;
-    }
-
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& state, stack<int>& st){
-        state[node] = 1;  // visiting
-        
-        for (int nxt : adj[node]) {
-            if (state[nxt] == 0) {
-                dfs(nxt, adj, state, st);
-            }
-        }
-        st.push(node);
-    }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses, 0);
         vector<vector<int>> adj(numCourses);
-        
-        // create directed graph: a → b
-        for (auto &p : prerequisites)
-            adj[p[0]].push_back(p[1]);
-        
-        vector<int> state(numCourses, 0); // 0=unvisited,1=visiting,2=done
-        
-        // run dfs on each component
-        for (int i = 0; i < numCourses; i++) {
-            if (state[i] == 0) {
-                if (!dfs(i, adj, state)) 
-                    return {};   // cycle detected
+        queue<int> q;
+        vector<int> s;
+        for(int i = 0; i < prerequisites.size(); i++){
+            int u = prerequisites[i][0]; int v = prerequisites[i][1];
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
+        for(int i = 0; i < indegree.size(); i++){
+            if(indegree[i] == 0){
+                q.push(i);
             }
         }
-        vector<int> state2(numCourses, 0);
-        stack<int> st; vector<int> res;
-        for (int i = 0; i < numCourses; i++) {
-            if (state2[i] == 0) {
-                dfs(i, adj, state2, st);
+        
+        
+        while(!q.empty()){
+            int t = q.front(); q.pop();
+            s.push_back(t);
+            for(int neigh : adj[t]){
+                indegree[neigh]--;
+                if(indegree[neigh] == 0){
+                    q.push(neigh);
+                }
             }
         }
-        while(!st.empty()){
-            res.push_back(st.top()); st.pop();
+        if(s.size() == numCourses){
+            return s;
         }
-        reverse(res.begin(), res.end());
-        return res;
+        return {};
     }
 };
