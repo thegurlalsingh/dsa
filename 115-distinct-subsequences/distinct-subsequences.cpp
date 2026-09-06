69233387dee117820550da11de4cd5a6fae13bb9
Gurlal Singh <g0390076@gmail.com>
2026-09-06 12:39:12 +0530

    Time: 34 ms (36.99%) | Memory: 27 MB (52.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,30 +1,31 @@
 class Solution {
-    int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp){
-        if(j >= t.size()){
-            return 1;
+    int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+        if (j == t.size() - 1) { 
+            int ans = 0;
+            for (int k = i; k < s.size(); k++) {
+                if (s[k] == t[j]) {
+                    ans++; // there are cases where last char of t can match with multiple ending chars of s, so counting all of them and returning
+                }
+            }
+            return ans;
         }
-
-        if(i >= s.size()){
+        if (i >= s.size()) {
             return 0;
-        } // i forgot to define this base case
-
+        }
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        int sk = solve(i + 1, j, s, t, dp); // we dont maximization here, if does not matches skip that char in main str not in pattern
-        int ta = 0;
-        if(s[i] == t[j]){
-            ta = solve(i + 1, j + 1, s, t, dp);
+        int skip = solve(i + 1, j, s, t, dp);
+        int take = 0;
+        if (s[i] == t[j]) {
+            take = solve(i + 1, j + 1, s, t, dp);
         }
-
-        return dp[i][j] = sk + ta;
+        return dp[i][j] = take + skip;
     }
+
 public:
-    int numDistinct(string s, string t) {
-        // if(s.size() < t.size()){
-        //     swap(s, t);
-        // }
+    int numDistinct(string s, string t) { 
         vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
-        return solve(0, 0, s, t, dp);
+        return solve(0, 0, s, t, dp); 
     }
 };
