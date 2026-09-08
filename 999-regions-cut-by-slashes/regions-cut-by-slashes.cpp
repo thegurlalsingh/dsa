fc2de88f2f13b45abe75d3f9d6046f2ef9581a81
Gurlal Singh <g0390076@gmail.com>
2026-09-08 19:33:01 +0530

    Time: 24 ms (9.95%) | Memory: 21.9 MB (7.46%) - LeetSync

--- 999-regions-cut-by-slashes/regions-cut-by-slashes.cpp
+++ 999-regions-cut-by-slashes/regions-cut-by-slashes.cpp
@@ -1,44 +1,85 @@
 class Solution {
-    void dfs(int i, int j, vector<vector<int>>& mat){
-        int n = mat.size();
-        if(i < 0 || j < 0 || i >= mat.size() || j >= mat.size() || mat[i][j] == 1){
-            return ;
-        }
+    vector<pair<int,int>> dir = {
+        {-1, 0},  
+        {1, 0},   
+        {0, -1},  
+        {0, 1}    
+    };
 
-        mat[i][j] = 1;
+    void helper(vector<vector<int>>& f, vector<vector<int>>& edges, int i, int j){
+        int x = i * 3;
+        int y = j * 3;
         
-        dfs(i + 1, j, mat);
-        dfs(i, j + 1, mat);
-        dfs(i - 1, j, mat);
-        dfs(i, j - 1, mat);
+        int m = 0; int n = 0;
+
+        for(int k = x; k < x + 3; k++){
+            n = 0;
+            for(int l = y; l < y + 3; l++){
+                edges[k][l] = f[m][n];
+                n++;
+            }
+            m++;
+        }
+    }
+
+    void solve(vector<vector<int>>& vis, vector<vector<int>>& edges, int i, int j){
+        int m = edges.size(); int n = edges[0].size();
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        vis[i][j] = 1;
+
+        while(!q.empty()){
+            auto [r, c] = q.front(); q.pop();
+
+            for (auto [dr, dc] : dir) {
+                int nr = r + dr;
+                int nc = c + dc;
+                if(nr < m && nc < n && nr >= 0 && nc >= 0 && edges[nr][nc] != 1 && !vis[nr][nc]){
+                    vis[nr][nc] = 1;
+                    q.push({nr, nc});
+                }
+
+            }
+        }
     }
 public:
     int regionsBySlashes(vector<string>& grid) {
-        int n = grid.size();
-        vector<vector<int>> mat(3 * n, vector<int>(3 * n, 0));
-        for(int i = 0; i < n; i++){
+        unordered_map<char, vector<vector<int>>> mp;
+        mp['/'] = {{0,0,1}, {0,1,0}, {1,0,0}};
+        mp[' '] = {{0,0,0}, {0,0,0}, {0,0,0}};
+        mp['\\'] = {{1,0,0}, {0,1,0}, {0,0,1}};
+
+        int m = grid.size();
+        int n = 0;
+        for(int i = 0; i < grid[0].size(); ){
+            if(grid[0][i] == '/' || grid[0][i] == ' '){
+                i++;
+            }
+            else{
+                i++;
+            }
+            n++;
+        }
+        m = m * 3;
+        n = n * 3;
+        vector<vector<int>> edges(m, vector<int>(n, 0));
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                helper(mp[grid[i][j]], edges, i, j);
+            }
+        }
+
+        int regions = 0;
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+        for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == '/'){
-                    mat[3 * i][3 * j + 2] = 1;
-                    mat[3 * i + 1][3 * j + 1] = 1;
-                    mat[3 * i + 2][3 * j] = 1;
-                }
-                else if(grid[i][j] == '\\'){
-                    mat[3 * i][3 * j] = 1;
-                    mat[3 * i + 1][3 * j + 1] = 1;
-                    mat[3 * i + 2][3 * j + 2] = 1;
+                if(edges[i][j] == 0 && !visited[i][j]){
+                    solve(visited, edges, i, j);
+                    regions++;
                 }
             }
         }
-        int ans = 0;
-        for(int i = 0; i < 3 * n; i++){
-            for(int j = 0; j < 3 * n; j++){
-                if(!mat[i][j]){
-                    ans++;
-                    dfs(i, j, mat);
-                }
-            }
-        }
-        return ans;
+
+        return regions;
     }
 };
