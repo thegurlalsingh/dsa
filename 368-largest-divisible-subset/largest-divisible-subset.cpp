de91c07fe4d0ca5cd9af775c307e1dcefc643f20
Gurlal Singh <g0390076@gmail.com>
2026-09-18 08:43:42 +0530

    Time: 19 ms (17.15%) | Memory: 12.5 MB (95.84%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,41 +1,42 @@
 class Solution {
-public:
-    vector<int> largestDivisibleSubset(vector<int>& nums) {
+    vector<int> solve(int i, int prev, vector<int>& nums, vector<int>& parent) {
         int n = nums.size();
-
-        sort(nums.begin(), nums.end());
-
         vector<int> dp(n, 1);
-        vector<int> parent(n, -1);
-
-        int maxLen = 1;
-        int lastIdx = 0;
-
         for (int i = 0; i < n; i++) {
+            parent[i] = -1;
             for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0) {
-                    if (dp[j] + 1 > dp[i]) {
-                        dp[i] = dp[j] + 1;
-                        parent[i] = j;
-                    }
+                if (((nums[j] % nums[i] == 0) || (nums[i] % nums[j] == 0)) && dp[j] + 1 > dp[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    parent[i] = j;
                 }
             }
+        }
+        return dp;
+    }
 
-            if (dp[i] > maxLen) {
-                maxLen = dp[i];
-                lastIdx = i;
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> parent(nums.size(), -1);
+        vector<int> dp = solve(0, -1, nums, parent);
+        int peakElem = -1; int peak = -1;
+        for(int i = 0; i < dp.size(); i++){
+            if(dp[i] > peakElem){
+                peakElem = dp[i];
+                peak = i;
+            }
+            else if(dp[i] == peakElem){
+                peak = i;
             }
         }
 
         vector<int> ans;
-
-        while (lastIdx != -1) {
-            ans.push_back(nums[lastIdx]);
-            lastIdx = parent[lastIdx];
+        while(peak != -1){
+            ans.push_back(nums[peak]);
+            peak = parent[peak];
         }
 
         reverse(ans.begin(), ans.end());
-
         return ans;
     }
 };
