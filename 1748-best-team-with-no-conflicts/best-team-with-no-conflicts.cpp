faf153f00cd2e514b5c34ae1caf4e03be61e2fac
Gurlal Singh <g0390076@gmail.com>
2026-09-22 00:36:39 +0530

    Time: 215 ms (31.53%) | Memory: 138.3 MB (15.53%) - LeetSync

--- 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
+++ 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
@@ -1,26 +1,32 @@
 class Solution {
-    int solve(int i, vector<pair<int, int>>& v, int prev, vector<vector<int>>& dp){
-        if(i >= v.size()){
+    int solve(int i, int prev, vector<pair<int, int>>& p, vector<vector<int>>& dp){
+        if(i >= p.size()){
             return 0;
         }
         if(dp[i][prev + 1] != -1){
             return dp[i][prev + 1];
         }
-        int s = solve(i + 1, v, prev, dp);
-        int t = 0;
-        if(prev == -1 || (v[i].second >= v[prev].second)){
-            t = v[i].second + solve(i + 1, v, i, dp);
+
+        int skip = solve(i + 1, prev, p, dp);
+        int take = 0;
+        if(prev == -1 || (p[prev].first <= p[i].first)){
+            take = p[i].first + solve(i + 1, i, p, dp);
         }
-        return dp[i][prev + 1] = max(t, s);
+        
+        return dp[i][prev + 1] = max(take, skip);
     }
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        vector<vector<int>> dp(ages.size(), vector<int>(ages.size() + 1, -1));
-        vector<pair<int, int>> v;
-        for(int i = 0; i < ages.size(); i++){
-            v.push_back({ages[i], scores[i]});
+        vector<pair<int, int>> p;
+        for(int i = 0; i < scores.size(); i++){
+            p.push_back({scores[i], ages[i]}); // {score, age}
         }
-        sort(v.begin(), v.end());
-        return solve(0, v, -1, dp);
+        sort(p.begin(), p.end(), [](auto& a, auto& b) {
+            if(a.second == b.second) // if age same prefer sort ascending in score
+                return a.first < b.first;
+            return a.second < b.second; // else sort ascending in age 
+        });
+        vector<vector<int>> dp(scores.size() + 1, vector<int>(ages.size() + 1, -1));
+        return solve(0, -1, p, dp);
     }
 };
