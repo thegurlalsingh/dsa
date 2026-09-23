39e911d742e9dd575b5e0bea9c40f0b2e615cb99
Gurlal Singh <g0390076@gmail.com>
2026-09-23 08:34:44 +0530

    Time: 85 ms (31.43%) | Memory: 100.6 MB (21.75%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,29 +1,48 @@
 class Solution {
-    int solve(int i, vector<tuple<int, int, int>>& v, vector<int>& dp, int prev){
-        if(i >= v.size()){
+    int bs(int i, vector<vector<int>>& p) {
+        int target = p[i][1];
+        int l = i + 1;
+        int r = p.size() - 1;
+        int ans = p.size();
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (p[mid][0] >= target) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    int solve(int i, vector<vector<int>>& p, vector<int>& dp) {
+        if (i >= p.size()) {
             return 0;
         }
-        if(dp[i] != -1){
+        if (dp[i] != -1) {
             return dp[i];
         }
-
-        int skip = solve(i + 1, v, dp, prev);
-        
-        int x = get<1>(v[i]);
-        int idx = lower_bound(v.begin(), v.end(), make_tuple(x, 0, 0)) - v.begin();
-
-        int take = get<2>(v[i]) + solve(idx, v, dp, i);
-    
+        int skip = solve(i + 1, p, dp);
+        int nextIndex = bs(i, p);
+        int take = p[i][2] + solve(nextIndex, p, dp);
+        // if(nextIndex != p.size()){
+            // take = p[i][2] + solve(nextIndex, p, dp);
+        // }
         return dp[i] = max(take, skip);
     }
+
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        vector<tuple<int, int, int>> v;
-        for(int i = 0; i < startTime.size(); i++){
-            v.push_back({startTime[i], endTime[i], profit[i]});
+        vector<vector<int>> p;
+        for (int i = 0; i < startTime.size(); i++) {
+            p.push_back({startTime[i], endTime[i], profit[i]});
         }
-        sort(v.begin(), v.end());
+        sort(p.begin(), p.end());
         vector<int> dp(startTime.size(), -1);
-        return solve(0, v, dp, -1);
+        return solve(0, p, dp);
     }
 };
