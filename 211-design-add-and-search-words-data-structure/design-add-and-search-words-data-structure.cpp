08b1a196a1ba83a81dc2ee94deb34d9bd9eba0bc
Gurlal Singh <g0390076@gmail.com>
2026-09-25 08:15:14 +0530

    Time: 900 ms (6.94%) | Memory: 712.9 MB (5.20%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -1,70 +1,76 @@
-class Trie {
+class Trie{
+    public:
     struct Node {
-        Node* links[26];
-        bool isEnd;
-        Node() {
-            for (int i = 0; i < 26; i++) {
-                links[i] = nullptr;
-            }
+        Node* child[26]; bool isEnd;
+        Node(){
             isEnd = false;
+            for(int i = 0; i < 26; i++){
+                child[i] = nullptr;
+            }
         }
     };
 
     Node* root;
 
-public:
-    Trie() { root = new Node(); }
-
-    void insert(string word) {
-        Node* node = root;
-        for (char ch : word) {
-            int idx = ch - 'a';
-            if (node->links[idx] == nullptr) {
-                node->links[idx] = new Node();
-            }
-            node = node->links[idx];
-        }
-        node->isEnd = true;
+    Trie(){
+        root = new Node();
     }
 
-    bool dfs(string& word, int i, Node* node) {
-        if (i == word.size()) {
-            return node->isEnd;
-        }
-        if (word[i] != '.') {
-            int idx = word[i] - 'a';
-            if (node->links[idx] == NULL) {
-                return false;
+    void insert(string word){
+        Node* curr = root;
+        for(char ch : word){
+            int idx = ch - 'a';
+            if(curr->child[idx] == nullptr){
+                curr->child[idx] = new Node();
             }
-            return dfs(word, i + 1, node->links[idx]);
-        } else {
-            for (char c = 'a'; c <= 'z'; c++) {
-                int idx = c - 'a';
-                if (node->links[idx] != NULL) {
-                    // word[i] = c;
-                    if (dfs(word, i + 1, node->links[idx])) {
+            curr = curr->child[idx];
+        }
+        curr->isEnd = true;
+    }
+
+    bool search(string word){
+        Node* curr = root;
+        
+        for(int i = 0; i < word.size(); i++){
+            if(word[i] == '.'){
+                for(char j = 'a'; j <= 'z'; j++){
+                    string s = word;
+                    s[i] = j;
+                    if(search(s)){
                         return true;
                     }
                 }
+                return false;
             }
+            int idx = word[i] - 'a';
+            if(curr->child[idx] == nullptr){
+                return false;
+            }
+            curr = curr->child[idx];
         }
-        return false;
+        return curr->isEnd;
     }
 
-    bool search(string word) {
-        Node* node = root;
-        return dfs(word, 0, node);
-    }
+    // bool dfs(string& s){
+
+    // }
 };
+
 
 class WordDictionary {
 public:
     Trie t;
-    WordDictionary() {}
-
-    void addWord(string word) { t.insert(word); }
-
-    bool search(string word) { return t.search(word); }
+    WordDictionary() {
+        
+    }
+    
+    void addWord(string word) {
+        t.insert(word);
+    }
+    
+    bool search(string word) {
+        return t.search(word);
+    }
 };
 
 /**
