a460e74aa2955559adc93895439f5ee44e170379
Gurlal Singh <g0390076@gmail.com>
2026-09-25 08:39:15 +0530

    Time: 87 ms (11.41%) | Memory: 34.6 MB (32.88%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
@@ -1,116 +1,30 @@
-class Trie {
-
-    struct Node {
-        Node* links[26];
-
-        bool isEnd;
-
-        Node() {
-            for (int i = 0; i < 26; i++)
-                links[i] = NULL;
-
-            isEnd = false;
-        }
-    };
-
-    Node* root;
-
-public:
-    Trie() { root = new Node(); }
-
-    void insert(string word) {
-
-        Node* node = root;
-
-        for (char ch : word) {
-
-            int idx = ch - 'a';
-
-            if (node->links[idx] == NULL)
-                node->links[idx] = new Node();
-
-            node = node->links[idx];
-        }
-
-        node->isEnd = true;
-    }
-
-    bool search(string word) {
-
-        Node* node = root;
-
-        for (char ch : word) {
-
-            int idx = ch - 'a';
-
-            if (node->links[idx] == NULL)
-                return false;
-
-            node = node->links[idx];
-        }
-
-        return node->isEnd;
-    }
-
-    bool startsWith(string prefix) {
-
-        Node* node = root;
-
-        for (char ch : prefix) {
-
-            int idx = ch - 'a';
-
-            if (node->links[idx] == NULL)
-                return false;
-
-            node = node->links[idx];
-        }
-
-        return true;
-    }
-};
-
 class Solution {
 public:
     string longestWord(vector<string>& words) {
-        Trie trie;
-
-        for (int i = 0; i < words.size(); i++) {
-            trie.insert(words[i]);
+        unordered_set<string> s;
+        for(int i = 0; i < words.size(); i++){
+            s.insert(words[i]);
         }
-        
-        string longest = "";
 
-        for (int i = 0; i < words.size(); i++) {
-
-            string word = words[i];
-
-            vector<string> pre(word.size());
-
-            pre[0] = string(1, word[0]);
-
-            for (int j = 1; j < word.size(); j++) {
-                pre[j] = pre[j - 1] + word[j];
-            }
-
+        string ans = "";
+        s.insert("");
+        for(int i = 0; i < words.size(); i++){
+            string temp = words[i].substr(0, words[i].size() - 1);
             bool found = true;
-
-            for (int j = 0; j < pre.size(); j++) {
-                if (!trie.search(pre[j])) {
+            for(int j = 1; j <= temp.size(); j++){
+                string t = temp.substr(0, j);
+                if(s.find(t) == s.end()){
                     found = false;
                     break;
                 }
             }
-
-            if (found) {
-
-                if (word.size() > longest.size()) {
-                    longest = word;
-                } else if (word.size() == longest.size() && word < longest) {
-                    longest = word;
+            if(found){
+                if(ans == "" || words[i].size() > ans.size() || (words[i].size() == ans.size() && words[i] < ans)){
+                    ans = words[i];
                 }
             }
         }
-        return longest;
+
+        return ans;
     }
 };
